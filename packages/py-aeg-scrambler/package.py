35b5e3ac47e4ae7fbc22d6957f3fc94cf364b405
KMace <kyle.mace@hotmail.co.uk>
2023-05-23 15:40:28 +0100

    Added dependencies

--- packages/py-aeg-scrambler/package.py
+++ packages/py-aeg-scrambler/package.py
@@ -12,6 +12,10 @@ class AegScrambler(PythonPackage):
 
     version("0.0.1", commit="aec9152b76a561781456c5bfe03411c761fab598")
     
-    depends_on("py-pridict", type=("build", "run"))
+    #depends_on("py-pridict", type=("build", "run"))
     depends_on("py-pyranges", type=("build", "run"))
-    
+    depends_on("typer", type=("build", "run"))
+    depends_on("scipy", type=("build", "run"))
+    depends_on("scikit-learn", type=("build", "run"))
+    depends_on("pandas", type=("build", "run"))
+    depends_on("pyyaml", type=("build", "run"))
