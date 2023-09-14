b4a47b9a4d895de49b5cb91dde9dc9efd4e3726b
Michael Woolnough <mw31@sanger.ac.uk>
2023-09-14 17:00:32 +0100

    Add new dependencies for MAVEQC.

--- packages/r-maveqc/package.py
+++ packages/r-maveqc/package.py
@@ -33,3 +33,4 @@ class RMaveqc(RPackage):
     depends_on("r-htmltools@0.5.5:", type=("build", "run"))
     depends_on("r-reshape2", type=("build", "run"))
     depends_on("r-gtools", type=("build", "run"))
+    depends_on("r-sparkline", type=("build", "run"))
