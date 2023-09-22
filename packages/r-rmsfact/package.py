adc74230070115c97a8a7d57a4a03565ca8bad8d
Dan <d.elia@outlook.com>
2023-09-22 15:31:27 +0100

    Fixing format of files + removing temp files

--- packages/r-rmsfact/package.py
+++ packages/r-rmsfact/package.py
@@ -3,35 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install r-rmsfact
-#
-# You can edit this file again by typing:
-#
-#     spack edit r-rmsfact
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class RRmsfact(RPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Amazing Random Facts About the World's Greatest Hacker
+    
+    Display a randomly selected quote about Richard M. Stallman based on the collection in the 'GNU Octave' function 'fact()' which was aggregated by Jordi Gutiérrez Hermoso based on the (now defunct) site stallmanfacts.com (which is accessible only via <http://archive.org>).
+    """
 
-    # FIXME: Add a proper url for your package's homepage here.
     cran = "rmsfact"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
 
     version("0.0.3", sha256="a54d20c4e7b36b9d731deeddfb160d69c34e82f1091238541439dfa45d1e4217")
 
--- packages/r-rvenn/package.py
+++ packages/r-rvenn/package.py
@@ -3,34 +3,16 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install r-rvenn
-#
-# You can edit this file again by typing:
-#
-#     spack edit r-rvenn
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack.package import *
 
 
 class RRvenn(RPackage):
-    """FIXME: Put a proper description of your package here."""
+    """Set Operations for Many Sets
+    
+    Set operations for many sets. The base functions for set operations in R can be used for only two sets. This package uses 'purr' to find the union, intersection and difference of three or more sets. This package also provides functions for pairwise set operations among several sets. Further, based on 'ggplot2' and 'ggforce', a Venn diagram can be drawn for two or three sets. For bigger data sets, a clustered heatmap showing presence/absence of the elements of the sets can be drawn based on the 'pheatmap' package. Finally, enrichment test can be applied to two sets whether an overlap is statistically significant or not.
+    """
 
     cran = "RVenn"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
 
     version("1.1.0", sha256="c41a96dd4a9b51e7dcc8647cdbaa0faa704ab22d5b0c1d45e593a6b23b00d504")
 
