b14f0bf76ca8fc0321ca180af6083a6def7d2ce1
Fei Sang <fs18@sanger.ac.uk>
2023-10-18 09:55:43 +0100

    new file:   py-dnaio/package.py 	new file:   py-flit-core/package.py 	new file:   py-pyreadr/package.py 	new file:   py-pyyaml/package.py 	new file:   py-typing-extensions/package.py 	new file:   r-dimsum/build_note.txt 	new file:   r-dimsum/package.py 	new file:   r-shortread/build_note.txt 	new file:   r-shortread/package.py 	new file:   starcode/build_note.txt 	new file:   starcode/package.py

---
+++ packages/r-dimsum/package.py
@@ -0,0 +1,41 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class RDimsum(RPackage):
+    """An error model and pipeline for analyzing deep mutational scanning (DMS) data and diagnosing common experimental pathologies."""
+
+    homepage = "https://github.com/lehner-lab/DiMSum"
+    git = "https://github.com/lehner-lab/DiMSum"
+
+    version("1.3", tag="v1.3")
+
+    depends_on("fastqc@0.11.9")
+    depends_on("vsearch@2.22.1")
+    depends_on("starcode@1.4")
+    depends_on("zlib")
+
+    depends_on("py-cutadapt@2.5")
+
+    depends_on("r+X", type=("build", "run"))
+    depends_on("r-biostrings", type=("build", "run"))
+    depends_on("r-iranges", type=("build", "run"))
+    depends_on("r-shortread", type=("build", "run"))
+    depends_on("r-cairo", type=("build", "run"))
+    depends_on("r-cowplot", type=("build", "run"))
+    depends_on("r-data-table", type=("build", "run"))
+    depends_on("r-ggally", type=("build", "run"))
+    depends_on("r-ggplot2", type=("build", "run"))
+    depends_on("r-gridextra", type=("build", "run"))
+    depends_on("r-hexbin", type=("build", "run"))
+    depends_on("r-optparse", type=("build", "run"))
+    depends_on("r-plyr", type=("build", "run"))
+    depends_on("r-reshape2", type=("build", "run"))
+    depends_on("r-rmarkdown", type=("build", "run"))
+    depends_on("r-seqinr", type=("build", "run"))
+    depends_on("r-stringr", type=("build", "run"))
+    depends_on("r-devtools", type=("build", "run"))
---
+++ packages/starcode/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Starcode(Package):
+    """Starcode is a DNA sequence clustering software."""
+
+    homepage = "https://github.com/gui11aume/starcode"
+    git = "https://github.com/gui11aume/starcode"
+
+    version("1.4", tag="1.4")
+
+    # depends_on("gcc")
+
+    def install(self, spec, prefix):
+        make()
+        mkdir(prefix + "/bin")
+        install("starcode", prefix + "/bin")
