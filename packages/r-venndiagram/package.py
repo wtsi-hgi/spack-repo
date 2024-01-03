4f46a70b83465030539d6ef23c6d07c64e907bc9
Michael Woolnough <mw31@sanger.ac.uk>
2024-01-03 16:40:11 +0000

    Corrected r-venndiagram recipe.

--- packages/r-venndiagram/package.py
+++ packages/r-venndiagram/package.py
@@ -7,18 +7,14 @@ from spack.package import *
 
 
 class RVenndiagram(RPackage):
-    """Generate High-Resolution Venn and Euler Plots
-    
-    A set of functions to generate high-resolution Venn and Euler plots. Includes handling for several special cases, including two-case scaling, and extensive customization of plot shape and structure.
-    """
+	"""Generate High-Resolution Venn and Euler Plots
 
-    homepage = "https://cran.r-project.org/web/packages/VennDiagram"
-    
-    cran = "VennDiagram"
+	A set of functions to generate high-resolution Venn and Euler plots. Includes handling for several special cases, including two-case scaling, and extensive customization of plot shape and structure.
+	"""
 
-    # versions
-    version("1.7.3", md5="aacd2b2f97a705dba364e9b70704ad27")
-    
+	cran = "VennDiagram"
 
-    # dependencies
-    
+	version("1.7.3", md5="aacd2b2f97a705dba364e9b70704ad27")
+
+	depends_on("r@3.5.0:", type=("build", "run"))
+	depends_on("r-futile-logger", type=("build", "run"))
