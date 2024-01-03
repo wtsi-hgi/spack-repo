bf68098c1a6f82af675867689048dbc8e32ebd2a
Fei Sang <fs18@sanger.ac.uk>
2024-01-03 14:33:10 +0000

    update py-pyreadr

--- packages/py-pyreadr/package.py
+++ packages/py-pyreadr/package.py
@@ -3,64 +3,23 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install py-pyreadr
-#
-# You can edit this file again by typing:
-#
-#     spack edit py-pyreadr
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
 
 class PyPyreadr(PythonPackage):
-    """FIXME: Put a proper description of your package here."""
+    """A python package to read and write R RData and Rds files into/from pandas dataframes. It does not need to have R or other external dependencies installed."""
 
-    # FIXME: Add a proper url for your package's homepage here.
-    homepage = "https://ofajardo.github.io/pyreadr/_build/html/index.html"
+    homepage = "https://github.com/ofajardo/pyreadr"
     pypi = "pyreadr/pyreadr-0.4.4.tar.gz"
-
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers("github_user1", "github_user2")
 
     version("0.4.4", sha256="690a6d87f25b6b211bad0d73fe0c9be87718e62329b142d835eadd951982c6ad")
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on("python@2.X:2.Y,3.Z:", type=("build", "run"))
-    # depends_on("py-pip@X.Y:", type="build")
-    # depends_on("py-wheel@X.Y:", type="build")
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
-    depends_on("py-setuptools")
-    depends_on("py-cython")
+    depends_on("python@3.9.9")
+    
+    depends_on("py-setuptools", type="build")
+    depends_on("py-cython@0.29.36", type="build")
+    depends_on("py-wheel", type="build")
     # depends_on("py-flit-core", type="build")
     # depends_on("py-poetry-core", type="build")
 
-    # FIXME: Add additional dependencies if required.
-    # depends_on("py-foo", type=("build", "run"))
-
-    def global_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
-
-    def install_options(self, spec, prefix):
-        # FIXME: Add options to pass to setup.py install
-        # FIXME: If not needed, delete this function
-        options = []
-        return options
