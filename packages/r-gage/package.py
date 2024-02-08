3365d94511bcc530f3db29a8ead8cc1c596a49d2
Fei Sang <fs18@sanger.ac.uk>
2024-02-08 10:30:35 +0000

    update recipe for r-gage

--- packages/r-gage/package.py
+++ packages/r-gage/package.py
@@ -9,13 +9,17 @@ from spack.package import *
 class RGage(RPackage):
     """GAGE is a widely used method for gene set (enrichment or GSEA) or pathway analysis. """
 
-    homepage = "https://github.com/datapplab/gage"
-    git = "https://github.com/datapplab/gage"
+    #homepage = "https://github.com/datapplab/gage"
+    #git = "https://github.com/datapplab/gage"
 
-    version("2.47.1", commit="d99df9a")
+    bioc = "gage"
+    url = "https://bioconductor.org/packages/release/bioc/src/contrib/gage_2.52.0.tar.gz"
+
+    version("2.52.0", md5="ffb5ec419d5e08f9b31154202e0e04c8")
 
     depends_on("r@4:", type=("build", "run"))
     depends_on("r-keggrest", type=("build", "run"))
     depends_on("r-annotationdbi", type=("build", "run"))
     depends_on("r-go-db", type=("build", "run"))
+    depends_on("r-graph", type=("build", "run"))
 
