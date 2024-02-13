b346c5bad2747c22125f57ebeb0a57adfc6fa836
Michael Woolnough <mw31@sanger.ac.uk>
2024-02-13 09:40:59 +0000

    Added missing setuptools dependency for py-multiprocessing-on-dill.

--- packages/py-multiprocessing-on-dill/package.py
+++ packages/py-multiprocessing-on-dill/package.py
@@ -12,3 +12,5 @@ class PyMultiprocessingOnDill(PythonPackage):
     pypi = "multiprocessing_on_dill/multiprocessing_on_dill-3.5.0a4.tar.gz"
 
     version("3.5.0a4", sha256="d6d50c300ff4bd408bb71eb78725e60231039ee9b3d0d9bb7697b9d0e15045e7")
+
+    depends_on("py-setuptools", type="build")
