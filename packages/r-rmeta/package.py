0cf53cd8e09a98cbaceb88cc3b3810f3413fef70
Fei Sang <fs18@sanger.ac.uk>
2024-02-13 16:52:25 +0000

    add recipe of r-rmeta

---
+++ packages/r-rmeta/package.py
@@ -0,0 +1,15 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class RRmeta(RPackage):
+    """Functions for simple fixed and random effects meta-analysis for two-sample comparisons and cumulative meta-analyses. Draws standard summary plots, funnel plots, and computes summaries and tests for association and heterogeneity."""
+
+    cran = "rmeta"
+
+    version("3.0", md5="b14aa174ad5bb7b8abb413a5aa4eacc0")
+
