aebe5c79c6d72be6f2c5b563fe6818cf778cd7eb
Michael Woolnough <mw31@sanger.ac.uk>
2024-03-06 09:43:03 +0000

    Corrected dependencies for py-freesasa.

--- packages/py-freesasa/package.py
+++ packages/py-freesasa/package.py
@@ -13,20 +13,9 @@ class PyFreesasa(PythonPackage):
 
     version("2.2.1", sha256="5630e65d619cf6a062028d7c6297afe8c5b18c677c75f1c32a0938399f4f6850")
 
-    # FIXME: Only add the python/pip/wheel dependencies if you need specific versions
-    # or need to change the dependency type. Generic python/pip/wheel dependencies are
-    # added implicity by the PythonPackage base class.
-    # depends_on("python@2.X:2.Y,3.Z:", type=("build", "run"))
-    # depends_on("py-pip@X.Y:", type="build")
-    # depends_on("py-wheel@X.Y:", type="build")
-
-    # FIXME: Add a build backend, usually defined in pyproject.toml. If no such file
-    # exists, use setuptools.
     depends_on("py-setuptools", type="build")
-    # depends_on("py-hatchling", type="build")
-    # depends_on("py-flit-core", type="build")
-    # depends_on("py-poetry-core", type="build")
-
-    # FIXME: Add additional dependencies if required.
-    #depends_on("py-foo", type=("build", "run"))
+    depends_on("py-twine", type=("build", "run"))
+    depends_on("py-cython", type=("build", "run"))
+    depends_on("py-biopython", type=("build", "run"))
+    depends_on("py-nose", type=("build", "run"))
     depends_on("freesasa", type=("build", "run"))
