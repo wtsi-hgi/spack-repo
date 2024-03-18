5155ff902c96513a5513dc46cb0355a4cf21567a
Michael Woolnough <mw31@sanger.ac.uk>
2024-03-18 14:54:54 +0000

    Add missing dependencies for r-scpipe.

--- packages/r-scpipe/package.py
+++ packages/r-scpipe/package.py
@@ -63,3 +63,6 @@ class RScpipe(RPackage):
 	depends_on("r-zlibbioc", type=("build", "run"))
 	depends_on("r-testthat", type=("build", "run"))
 	depends_on("zlib", type=("build", "link", "run"))
+	depends_on("curl", type=("build", "link", "run"))
+	depends_on("bzip2", type=("build", "link", "run"))
+	depends_on("xz", type=("build", "link", "run"))
