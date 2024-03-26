515bb24e9dd4bb50cf0113e78abcfb23cc77b629
Michael Woolnough <mw31@sanger.ac.uk>
2024-03-26 15:59:04 +0000

    Add missing dependencies for r-rfastp.

--- packages/r-rfastp/package.py
+++ packages/r-rfastp/package.py
@@ -23,3 +23,7 @@ class RRfastp(RPackage):
 	depends_on("r-reshape2", type=("build", "run"))
 	depends_on("r-rhtslib", type=("build", "run"))
 	depends_on("r-zlibbioc", type=("build", "run"))
+	depends_on("curl", type=("build", "link", "run"))
+	depends_on("bzip2", type=("build", "link", "run"))
+	depends_on("xz", type=("build", "link", "run"))
+	depends_on("zlib", type=("build", "link", "run"))
