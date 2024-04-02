aba468c931f2aee9a82a3cd53a8144f645e141c7
Michael Woolnough <mw31@sanger.ac.uk>
2024-04-02 09:37:21 +0100

    Add missing dependency for r-hicseg.

--- packages/r-hicseg/package.py
+++ packages/r-hicseg/package.py
@@ -17,3 +17,4 @@ class RHicseg(RPackage):
 	version("1.1", md5="38e211ee2c32384a4c2b19f1ec986f51")
 
 	depends_on("r@2.10:", type=("build", "run"))
+	depends_on("gsl", type=("build", "link", "run"))
