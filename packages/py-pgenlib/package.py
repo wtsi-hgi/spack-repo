d8c0065aad3217e1b4ebd538f2084a0accb92bc1
Michael Woolnough <mw31@sanger.ac.uk>
2024-04-09 08:44:43 +0100

    Add missing setuptools dependency for py-pgenlib.

--- packages/py-pgenlib/package.py
+++ packages/py-pgenlib/package.py
@@ -13,6 +13,7 @@ class PyPgenlib(PythonPackage):
 
     version("0.90.1", sha256="069db673c1cc5baffcb9b20c2e6e26cc9fdaadd0095f55a188b41502d0511cb9")
 
+    depends_on("py-setuptools", type="build")
     depends_on("py-cython@0.29.21:", type=("build", "run"))
     depends_on("py-numpy@1.19.0:", type=("build", "run"))
     depends_on("zlib", type=("build", "link"))
