5554f130efb19a06395e5f1374c8aff0c11fb141
Michael Woolnough <mw31@sanger.ac.uk>
2024-04-22 11:17:32 +0100

    Correct metadata.

--- packages/tmscore/package.py
+++ packages/tmscore/package.py
@@ -5,16 +5,12 @@
 
 from spack.package import *
 
-
 class Tmscore(Package):
-    """Entire source tree for the UCSC Genome Browser Group's suite of biological analysis and web display programs as well as some of Jim Kent's own tools."""
+    """TM-score is a metric for assessing the topological similarity of protein structures. It is designed to solve two major problems in traditional metrics such as root-mean-square deviation (RMSD): (1) TM-score weights smaller distance errors stronger than larger distance errors and makes the score value more sensitive to the global fold similarity than to the local structural variations; (2) TM-score introduces a length-dependent scale to normalize the distance errors and makes the magnitude of TM-score length-independent for random structure pairs. TM-score has the value in (0,1], where 1 indicates a perfect match between two structures. Following strict statistics of structures in the PDB, scores below 0.17 correspond to randomly chosen unrelated proteins whereas structures with a score higher than 0.5 assume generally the same fold in SCOP/CATH. The equation of TM-score can be found at Wikipedia or the original publication. Please read "Problem and Solution", if you have problem on how to calculate TM-score for protein/RNA/DNA structures."""
 
-    homepage = "https://github.com/ucscGenomeBrowser/kent"
-    git = "https://github.com/ucscGenomeBrowser/kent/"
+    homepage = "https://seq2fun.dcmb.med.umich.edu//TM-score/"
     
     version("2022-02-27", sha256="0058ba45be81c56a187f72ca30c11c8127ed96dffd158a9b6caa964c680c8f86", url="https://seq2fun.dcmb.med.umich.edu//TM-score/TMscore.cpp", expand=False)
-
-    #depends_on("libm", type=("build", "link"))
 
     def install(self, spec, prefix):
         bin = join_path(self.prefix, "bin")
