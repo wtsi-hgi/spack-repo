f1910f69a843a7a7ad890fc88d0ec7c84c8c51e3
Michael Woolnough <mw31@sanger.ac.uk>
2024-05-01 16:07:25 +0100

    py-setuptools should not be a runtime dependency.

--- packages/py-pyfaidx/package.py
+++ packages/py-pyfaidx/package.py
@@ -14,5 +14,5 @@ class PyPyfaidx(PythonPackage):
     version("0.8.1.1", sha256="6f0482352619f2cc56003ca22321bdb0d0764b656795bc1e4062b1fa9b08686b")
     version("0.5.5.2", sha256="9ac22bdc7b9c5d995d32eb9dc278af9ba970481636ec75c0d687d38c26446caa")
 
-    depends_on("py-setuptools@0.7:", type=("build", "run"))
+    depends_on("py-setuptools@0.7:", type=("build"))
     depends_on("py-six", type=("build", "run"))
