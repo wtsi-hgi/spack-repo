7771a65cda01b0eebf947fc8cfcc8eec465761dd
Michael Woolnough <mw31@sanger.ac.uk>
2024-05-01 14:20:17 +0100

    Add older version for r-s4arrays.

--- packages/r-s4arrays/package.py
+++ packages/r-s4arrays/package.py
@@ -18,7 +18,7 @@ class RS4arrays(RPackage):
 
 	version("1.2.1", md5="56e78d721cbf2dc16d12123dca444272")
 	version("1.2.0", md5="c77474b839251d19d31708831de3c1bf")
-	version("1.1.6", commit="820ba8cd6c9d2d39b141cd6f00a8de07cc2fe3e4")
+	version("0.4.1", commit="09d02b41fbf2a514f176e740ee7ab7bbe2b60151")
 
 	depends_on("r@4.3:", type=("build", "run"), when="@1.2:")
 	depends_on("r-matrix", type=("build", "run"))
