4b17db54491d62200c34f1222428a14e094db8c8
Michael Woolnough <mw31@sanger.ac.uk>
2024-05-02 10:08:19 +0100

    Add older version for r-seuratwrappers.

--- packages/r-seuratwrappers/package.py
+++ packages/r-seuratwrappers/package.py
@@ -13,6 +13,7 @@ class RSeuratwrappers(RPackage):
 	git = "https://github.com/satijalab/seurat-wrappers.git"
 
 	version("2024-01-29", commit="d9594f67a9eab4f917390010a1c106b5422b102e")
+	version("2023-04-18", commit="f647ce440a017f9f836eef5dac88f4c414370e94")
 
 	depends_on("r-biocmanager", type=("build", "run"))
 	depends_on("r-cowplot", type=("build", "run"))
@@ -21,6 +22,7 @@ class RSeuratwrappers(RPackage):
 	depends_on("r-matrix", type=("build", "run"))
 	depends_on("r-remotes", type=("build", "run"))
 	depends_on("r-rsvd", type=("build", "run"))
-	depends_on("r-seurat", type=("build", "run"))
+	depends_on("r-seurat@4.2.0:", type=("build", "run"), when="@2023-04-18:")
+	depends_on("r-seurat@5.0.0:", type=("build", "run"), when="@2024-01-29:")
 	depends_on("r-rlang", type=("build", "run"))
 	depends_on("r-r-utils", type=("build", "run"))
