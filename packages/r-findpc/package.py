b5856b54928833f6cb390fb46c2c1c565b06c275
Dan <d.elia@outlook.com>
2024-05-07 15:33:53 +0100

    Add recipe for r-findpc

---
+++ packages/r-findpc/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class RFindpc(RPackage):
+	"""The goal of ggsankey is to make beautiful sankey, alluvial and sankey bump plots in ggplot2."""
+	
+	homepage = "https://github.com/haotian-zhuang/findPC"
+	git = "https://github.com/haotian-zhuang/findPC.git"
+
+	version("2022-06-27", commit="0e85c94124a4dd6037b4fecb0558450b0301b512")
+
+	depends_on("r@2.10:", type=("build", "run"))
+	depends_on("r-ggplot2", type=("build", "run"))
+	depends_on("r-patchwork", type=("build", "run"))
+	depends_on("r-reshape2", type=("build", "run"))
+	depends_on("r-rmarkdown", type=("build", "run"))
+	depends_on("r-knitr", type=("build", "run"))
