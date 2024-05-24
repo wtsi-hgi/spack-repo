3798b88e45d5505fd89cfb28b7c756ef82b7249b
Michael Woolnough <mw31@sanger.ac.uk>
2024-05-24 16:47:16 +0100

    Add recipe for reduce.

---
+++ packages/reduce/package.py
@@ -0,0 +1,24 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+class Reduce(CMakePackage):
+    """Reduce - tool for adding and correcting hydrogens in PDB files"""
+
+    homepage = "https://github.com/rlabduke/reduce"
+    url = "https://github.com/rlabduke/reduce/archive/refs/tags/v4.14.tar.gz"
+
+    version("4.14", sha256="62e61cce221fff76b5834031302d91fe703a19945a42e16620d4fb860604daf4")
+
+    depends_on("boost", type=("build", "link"))
+    depends_on("python", type=("build", "link", "run"))
+
+    def cmake_args(self):
+        # FIXME: Add arguments other than
+        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
+        # FIXME: If not needed delete this function
+        args = []
+        return args
