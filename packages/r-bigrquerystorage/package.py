254f6c3085dd15e90ef2df6efcfe4655acf5440a
Eric Hidari <feedbackeric@gmail.com>
2024-06-17 09:30:07 +0000

    fix r-bigrquerystorage

--- packages/r-bigrquerystorage/package.py
+++ packages/r-bigrquerystorage/package.py
@@ -7,27 +7,27 @@ from spack.package import *
 
 
 class RBigrquerystorage(RPackage):
-	"""An Interface to Google's 'BigQuery Storage' API
+    """An Interface to Google's 'BigQuery Storage' API
 
-	Easily talk to Google's 'BigQuery Storage' API from R
+        Easily talk to Google's 'BigQuery Storage' API from R
     (<https://cloud.google.com/bigquery/docs/reference/storage/rpc>).
-	"""
-	
-	homepage = "https://github.com/meztez/bigrquerystorage"
-	cran = "bigrquerystorage" 
+    """
 
-	version("1.1.0", md5="802c8d8fec57d3fabc47072eec69d232")
-	version("1.0.0", md5="f9d70806e386e18cfea1ba86de943106")
+    homepage = "https://github.com/meztez/bigrquerystorage"
+    cran = "bigrquerystorage"
 
-	depends_on("r-arrow", type=("build", "run"))
-	depends_on("r-rlang", type=("build", "run"))
-	depends_on("r-dbi", type=("build", "run"))
-	depends_on("r-bigrquery", type=("build", "run"))
-	depends_on("r-assertthat", type=("build", "run"))
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-lifecycle", type=("build", "run"))
-	depends_on("r-bit64", type=("build", "run"))
-	depends_on("r-tibble", type=("build", "run"))
-	depends_on("protobuf", type=("build", "link", "run"))
-	depends_on("grpc", type=("build", "link", "run"))
-	depends_on("openssl", type=("build", "link", "run"))
+    version("1.1.0", md5="802c8d8fec57d3fabc47072eec69d232")
+    version("1.0.0", md5="f9d70806e386e18cfea1ba86de943106")
+
+    depends_on("r-arrow", type=("build", "run"))
+    depends_on("r-rlang", type=("build", "run"))
+    depends_on("r-dbi", type=("build", "run"))
+    depends_on("r-bigrquery", type=("build", "run"))
+    depends_on("r-assertthat", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-lifecycle", type=("build", "run"))
+    depends_on("r-bit64", type=("build", "run"))
+    depends_on("r-tibble", type=("build", "run"))
+    depends_on("protobuf", type=("build", "link", "run"))
+    depends_on("grpc +shared", type=("build", "link", "run"))
+    depends_on("openssl", type=("build", "link", "run"))
