e5e0f336c97fdd272a10668dee4e84cae1469dc7
Eric Hidari <feedbackeric@gmail.com>
2024-06-27 07:31:41 +0000

    Patch rust version checker

--- packages/r-fangs/package.py
+++ packages/r-fangs/package.py
@@ -7,15 +7,23 @@ from spack.package import *
 
 
 class RFangs(RPackage):
-	"""Feature Allocation Neighborhood Greedy Search Algorithm
+    """Feature Allocation Neighborhood Greedy Search Algorithm
 
-	A neighborhood-based, greedy search algorithm is performed to estimate a feature allocation by minimizing the expected loss based on posterior samples from the feature allocation distribution. The method is currently under peer review but an earlier draft is available in Dahl, Johnson, and Andros (2022+) <doi:10.48550/arXiv.2207.13824>.
-	"""
-	
-	homepage = "https://github.com/dbdahl/fangs"
-	cran = "fangs" 
+    A neighborhood-based, greedy search algorithm is performed to estimate a feature allocation by minimizing the expected loss based on posterior samples from the feature allocation distribution. The method is currently under peer review but an earlier draft is available in Dahl, Johnson, and Andros (2022+) <doi:10.48550/arXiv.2207.13824>.
+    """
 
-	version("0.2.13", md5="65885038704f1aa61672560844008dd7")
+    homepage = "https://github.com/dbdahl/fangs"
+    cran = "fangs"
 
-	depends_on("r@4.2:", type=("build", "run"))
-	depends_on("rust", type=("build", "link", "run"))
+    version("0.2.13", md5="65885038704f1aa61672560844008dd7")
+
+    depends_on("r@4.2:", type=("build", "run"))
+    depends_on("rust", type=("build", "link", "run"))
+
+    def patch(self):
+        filter_file(
+            'version <- strsplit(output, " ", fixed = TRUE)[[1]][2]',
+            'version <- strsplit(output, " ", fixed = TRUE)[[1]][2]\nversion <- sub("-nightly$", "", version)',
+            "tools/cargo_run.R",
+            string=True,
+        )
