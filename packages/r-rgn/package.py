8fe8f5ad2131621c762b6da6763182a083672237
Eric Hidari <feedbackeric@gmail.com>
2024-06-28 08:44:00 +0000

    set makefile jobs to 1

--- packages/r-rgn/package.py
+++ packages/r-rgn/package.py
@@ -7,18 +7,21 @@ from spack.package import *
 
 
 class RRgn(RPackage):
-	"""Robust-Gauss Newton (RGN) Optimization of Sum-of-Squares
-Objective Function
+    """Robust-Gauss Newton (RGN) Optimization of Sum-of-Squares
+    Objective Function
 
-	Implementation of the Robust Gauss-Newton (RGN) algorithm,
-    designed for solving optimization problems with a sum of least squares
-    objective function. For algorithm details please refer to Qin et. al. (2018) 
-    <doi:10.1029/2017WR022488>.
-	"""
-	
-	homepage = "https://github.com/ClimateAnalytics/RGN/"
-	cran = "RGN" 
+        Implementation of the Robust Gauss-Newton (RGN) algorithm,
+        designed for solving optimization problems with a sum of least squares
+        objective function. For algorithm details please refer to Qin et. al. (2018)
+        <doi:10.1029/2017WR022488>.
+    """
 
-	version("1.0.0", md5="8f2f56ec4e934857371a6c5face30931")
+    homepage = "https://github.com/ClimateAnalytics/RGN/"
+    cran = "RGN"
 
-	depends_on("r@3.5:", type=("build", "run"))
+    version("1.0.0", md5="8f2f56ec4e934857371a6c5face30931")
+
+    depends_on("r@3.5:", type=("build", "run"))
+
+    def setup_build_environment(self, env):
+        env.set("MAKEFLAGS", "-j1")
