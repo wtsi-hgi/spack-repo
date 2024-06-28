9743409cf71854744ea6ec458cdf5d10715d05e3
Eric Hidari <feedbackeric@gmail.com>
2024-06-28 09:31:33 +0000

    add an AC unique file signature

--- packages/r-glad/package.py
+++ packages/r-glad/package.py
@@ -7,17 +7,24 @@ from spack.package import *
 
 
 class RGlad(RPackage):
-	"""Gain and Loss Analysis of DNA
+    """Gain and Loss Analysis of DNA
 
-	Analysis of array CGH data : detection of breakpoints in genomic profiles and assignment of a status (gain, normal or loss) to each chromosomal regions identified.
-	"""
-	
-	homepage = "http://bioinfo.curie.fr"
-	bioc = "GLAD" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/GLAD_2.66.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/GLAD/GLAD_2.66.0.tar.gz"]
+    Analysis of array CGH data : detection of breakpoints in genomic profiles and assignment of a status (gain, normal or loss) to each chromosomal regions identified.
+    """
 
-	version("2.66.0", md5="f222e07624fb8b1c4780a4960b0f2008")
+    homepage = "http://bioinfo.curie.fr"
+    bioc = "GLAD"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/GLAD_2.66.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/GLAD/GLAD_2.66.0.tar.gz",
+    ]
 
-	depends_on("r@2.10:", type=("build", "run"))
-	depends_on("r-aws", type=("build", "run"))
-	depends_on("gsl", type=("build", "link", "run"))
+    version("2.66.0", md5="f222e07624fb8b1c4780a4960b0f2008")
+
+    depends_on("r@2.10:", type=("build", "run"))
+    depends_on("r-aws", type=("build", "run"))
+    depends_on("gsl", type=("build", "link", "run"))
+
+    @run_before("install")
+    def set_ac_unique_file(self):
+        touch("GLAD")
