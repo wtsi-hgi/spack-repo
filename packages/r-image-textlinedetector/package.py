20644be28c9a63eddfba704289064d6f0c69a3be
Eric Hidari <feedbackeric@gmail.com>
2024-07-01 08:38:49 +0000

    Adjust opencv dep settings

--- packages/r-image-textlinedetector/package.py
+++ packages/r-image-textlinedetector/package.py
@@ -7,20 +7,23 @@ from spack.package import *
 
 
 class RImageTextlinedetector(RPackage):
-	"""Segment Images in Text Lines and Words
+    """Segment Images in Text Lines and Words
 
-	Find text lines in scanned images and segment the lines into words.
+        Find text lines in scanned images and segment the lines into words.
     Includes implementations of the paper 'Novel A* Path Planning Algorithm for Line Segmentation of Handwritten Documents' by Surinta O. et al (2014) <doi:10.1109/ICFHR.2014.37> available at <https://github.com/smeucci/LineSegm>,
-    an implementation of 'A Statistical approach to line segmentation in handwritten documents' by Arivazhagan M. et al (2007) <doi:10.1117/12.704538>, 
+    an implementation of 'A Statistical approach to line segmentation in handwritten documents' by Arivazhagan M. et al (2007) <doi:10.1117/12.704538>,
     and a wrapper for an image segmentation technique to detect words in text lines as described in the paper 'Scale Space Technique for Word Segmentation in Handwritten Documents' by Manmatha R. and Srimal N. (1999) paper at <doi:10.1007/3-540-48236-9_3>, wrapper for code available at <https://github.com/arthurflor23/text-segmentation>.
-    Provides as well functionality to put cursive text in images upright using the approach defined in the paper 'A new normalization technique for cursive handwritten words' by  Vinciarelli A. and Luettin J. (2001) <doi:10.1016/S0167-8655(01)00042-3>. 
-	"""
-	
-	homepage = "https://github.com/DIGI-VUB/image.textlinedetector"
-	cran = "image.textlinedetector" 
+    Provides as well functionality to put cursive text in images upright using the approach defined in the paper 'A new normalization technique for cursive handwritten words' by  Vinciarelli A. and Luettin J. (2001) <doi:10.1016/S0167-8655(01)00042-3>.
+    """
 
-	version("0.2.3", md5="4176d6ec469750525404303e44c8c1d4")
+    homepage = "https://github.com/DIGI-VUB/image.textlinedetector"
+    cran = "image.textlinedetector"
 
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-magick", type=("build", "run"))
-	depends_on("opencv", type=("build", "link", "run"))
+    version("0.2.3", md5="4176d6ec469750525404303e44c8c1d4")
+
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-magick", type=("build", "run"))
+    depends_on("opencv +imgproc +highgui +objdetect", type=("build", "run", "link"))
+
+    def setup_build_environment(self, env):
+        env.set("PKG_CFLAGS", "-I{}".format(self.spec["opencv"].prefix.include.opencv4))
