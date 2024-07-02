bb3299d6de4f887734cbb2645886df93a14055a4
Eric Hidari <feedbackeric@gmail.com>
2024-07-02 13:47:50 +0000

    fix the recipe

--- packages/r-interactivedisplay/package.py
+++ packages/r-interactivedisplay/package.py
@@ -7,25 +7,32 @@ from spack.package import *
 
 
 class RInteractivedisplay(RPackage):
-	"""Package for enabling powerful shiny web displays of Bioconductor objects
+    """Package for enabling powerful shiny web displays of Bioconductor objects
 
-	The interactiveDisplay package contains the methods needed to generate interactive Shiny based display methods for Bioconductor objects.
-	"""
-	
-	bioc = "interactiveDisplay" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/interactiveDisplay_1.40.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/interactiveDisplay/interactiveDisplay_1.40.0.tar.gz"]
+    The interactiveDisplay package contains the methods needed to generate interactive Shiny based display methods for Bioconductor objects.
+    """
 
-	version("1.40.0", md5="4adec73e763df3dc2e697e41bff3ead8")
+    bioc = "interactiveDisplay"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/interactiveDisplay_1.40.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/interactiveDisplay/interactiveDisplay_1.40.0.tar.gz",
+    ]
 
-	depends_on("r@3.5:", type=("build", "run"))
-	depends_on("r-biocgenerics", type=("build", "run"))
-	depends_on("r-interactivedisplaybase@1.7.3:", type=("build", "run"))
-	depends_on("r-shiny", type=("build", "run"))
-	depends_on("r-rcolorbrewer", type=("build", "run"))
-	depends_on("r-ggplot2", type=("build", "run"))
-	depends_on("r-reshape2", type=("build", "run"))
-	depends_on("r-plyr", type=("build", "run"))
-	depends_on("r-gridsvg", type=("build", "run"))
-	depends_on("r-xml", type=("build", "run"))
-	depends_on("r-category", type=("build", "run"))
-	depends_on("r-annotationdbi", type=("build", "run"))
+    version("1.40.0", md5="4adec73e763df3dc2e697e41bff3ead8")
+
+    depends_on("r@3.5:", type=("build", "run"))
+    depends_on("r-biocgenerics", type=("build", "run"))
+    depends_on("r-interactivedisplaybase@1.7.3:", type=("build", "run"))
+    depends_on("r-shiny", type=("build", "run"))
+    depends_on("r-rcolorbrewer", type=("build", "run"))
+    depends_on("r-ggplot2", type=("build", "run"))
+    depends_on("r-reshape2", type=("build", "run"))
+    depends_on("r-plyr", type=("build", "run"))
+    depends_on("r-gridsvg", type=("build", "run"))
+    depends_on("r-xml", type=("build", "run"))
+    depends_on("r-category", type=("build", "run"))
+    depends_on("r-annotationdbi", type=("build", "run"))
+
+    def patch(self):
+        for i in ['if (!requireNamespace("BiocManager", quietly=TRUE))', 'install.packages("BiocManager")']:
+            filter_file(i, "", "R/interactiveDisplay.R", string=True)
