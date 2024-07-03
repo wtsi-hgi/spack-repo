6dee254eecb2257d6b19788a8c73c4f31b9eaddc
Eric Hidari <feedbackeric@gmail.com>
2024-07-03 16:21:27 +0000

    fix r-iplots

--- packages/r-iplots/package.py
+++ packages/r-iplots/package.py
@@ -7,16 +7,23 @@ from spack.package import *
 
 
 class RIplots(RPackage):
-	"""iPlots - Interactive Graphics for R
+    """iPlots - Interactive Graphics for R
 
-	Interactive plots for R.
-	"""
-	
-	homepage = "http://www.iPlots.org/"
-	cran = "iplots" 
+    Interactive plots for R.
+    """
 
-	version("1.1-8", md5="fc5a76df91251eaa4e4f4dd09008cf9c")
+    homepage = "http://www.iPlots.org/"
+    cran = "iplots"
 
-	depends_on("r@1.5:", type=("build", "run"))
-	depends_on("r-rjava@0.5.0:", type=("build", "run"))
-	depends_on("r-png", type=("build", "run"))
+    version("1.1-8", md5="fc5a76df91251eaa4e4f4dd09008cf9c")
+
+    depends_on("r@1.5:", type=("build", "run"))
+    depends_on("r-rjava@0.5.0:", type=("build", "run"))
+    depends_on("r-png", type=("build", "run"))
+    depends_on("libxtst", type=("build", "run", "link"))
+    depends_on("libxi", type=("build", "run", "link"))
+
+    def setup_build_environment(self, env):
+        env.set("NOAWT", "1")
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["libxtst"].prefix.lib)
+        env.prepend_path("LD_LIBRARY_PATH", self.spec["libxi"].prefix.lib)
