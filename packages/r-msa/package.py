e81890045bfac2abda93c47d98075b18d3971f28
Eric Hidari <feedbackeric@gmail.com>
2024-07-04 15:49:48 +0000

    fix r-lymphoseq

--- packages/r-msa/package.py
+++ packages/r-msa/package.py
@@ -7,20 +7,32 @@ from spack.package import *
 
 
 class RMsa(RPackage):
-	"""Multiple Sequence Alignment
+    """Multiple Sequence Alignment
 
-	The 'msa' package provides a unified R/Bioconductor interface to the multiple sequence alignment algorithms ClustalW, ClustalOmega, and Muscle. All three algorithms are integrated in the package, therefore, they do not depend on any external software tools and are available for all major platforms. The multiple sequence alignment algorithms are complemented by a function for pretty-printing multiple sequence alignments using the LaTeX package TeXshade.
-	"""
-	
-	homepage = "http://www.bioinf.jku.at/software/msa/"
-	bioc = "msa" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/msa_1.34.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/msa/msa_1.34.0.tar.gz"]
+    The 'msa' package provides a unified R/Bioconductor interface to the multiple sequence alignment algorithms ClustalW, ClustalOmega, and Muscle. All three algorithms are integrated in the package, therefore, they do not depend on any external software tools and are available for all major platforms. The multiple sequence alignment algorithms are complemented by a function for pretty-printing multiple sequence alignments using the LaTeX package TeXshade.
+    """
 
-	version("1.34.0", md5="78eba41452ffe497e1a7c72116335b3e")
+    homepage = "http://www.bioinf.jku.at/software/msa/"
+    bioc = "msa"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/msa_1.34.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/msa/msa_1.34.0.tar.gz",
+    ]
 
-	depends_on("r@3.3:", type=("build", "run"))
-	depends_on("r-biostrings@2.40:", type=("build", "run"))
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-biocgenerics", type=("build", "run"))
-	depends_on("r-iranges@1.20:", type=("build", "run"))
-	depends_on("r-s4vectors", type=("build", "run"))
+    version("1.34.0", md5="78eba41452ffe497e1a7c72116335b3e")
+
+    depends_on("r@3.3:", type=("build", "run"))
+    depends_on("r-biostrings@2.40:", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-biocgenerics", type=("build", "run"))
+    depends_on("r-iranges@1.20:", type=("build", "run"))
+    depends_on("r-s4vectors", type=("build", "run"))
+
+    def patch(self):
+        filter_file(
+            """#include <pthread.h>""",
+            """#define _GNU_SOURCE 1
+#include <pthread.h>""",
+            "src/gc-8.2.2/configure",
+            string=True,
+        )
