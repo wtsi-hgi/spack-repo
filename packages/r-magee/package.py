a9868937446a21a07abb46de6ed8f93e9d32af57
Eric Hidari <feedbackeric@gmail.com>
2024-07-05 10:49:57 +0000

    add missing dependencies

--- packages/r-magee/package.py
+++ packages/r-magee/package.py
@@ -7,23 +7,26 @@ from spack.package import *
 
 
 class RMagee(RPackage):
-	"""Mixed Model Association Test for GEne-Environment Interaction
+    """Mixed Model Association Test for GEne-Environment Interaction
 
-	Use a 'glmmkin' class object (GMMAT package) from the null model to perform generalized linear mixed model-based single-variant and variant set main effect tests, gene-environment interaction tests, and joint tests for association, as proposed in Wang et al. (2020) <DOI:10.1002/gepi.22351>.
-	"""
-	
-	cran = "MAGEE" 
+    Use a 'glmmkin' class object (GMMAT package) from the null model to perform generalized linear mixed model-based single-variant and variant set main effect tests, gene-environment interaction tests, and joint tests for association, as proposed in Wang et al. (2020) <DOI:10.1002/gepi.22351>.
+    """
 
-	version("1.3.2", md5="29eb11b0e7f407c790b29815145b1d6d")
+    cran = "MAGEE"
 
-	depends_on("r@3.2:", type=("build", "run"))
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-matrix", type=("build", "run"))
-	depends_on("r-mass", type=("build", "run"))
-	depends_on("r-foreach", type=("build", "run"))
-	depends_on("r-gmmat", type=("build", "run"))
-	depends_on("r-compquadform", type=("build", "run"))
-	depends_on("r-data-table", type=("build", "run"))
-	depends_on("r-rcpparmadillo", type=("build", "run"))
-	depends_on("bzip2", type=("build", "link", "run"))
-	depends_on("zlib", type=("build", "link", "run"))
+    version("1.4.1", sha256="fa5c4521d9bb034b90ed3407a0c49f562ddfa00352a9ed84a9b90b21e45e6a76")
+    version("1.3.2", md5="29eb11b0e7f407c790b29815145b1d6d")
+
+    depends_on("r@3.2:", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-matrix", type=("build", "run"))
+    depends_on("r-mass", type=("build", "run"))
+    depends_on("r-foreach", type=("build", "run"))
+    depends_on("r-gmmat", type=("build", "run"))
+    depends_on("r-compquadform", type=("build", "run"))
+    depends_on("r-data-table", type=("build", "run"))
+    depends_on("r-rcpparmadillo", type=("build", "run"))
+    depends_on("bzip2", type=("build", "link", "run"))
+    depends_on("zlib", type=("build", "link", "run"))
+    depends_on("zstd", type=("build", "link", "run"))
+    depends_on("libdeflate", type=("build", "link", "run"))
