be51db82697b1819c5895a52939dd8e08286fda5
Eric Hidari <feedbackeric@gmail.com>
2024-07-09 15:46:45 +0000

    add open babel env vars

--- packages/r-chemmineob/package.py
+++ packages/r-chemmineob/package.py
@@ -7,20 +7,27 @@ from spack.package import *
 
 
 class RChemmineob(RPackage):
-	"""R interface to a subset of OpenBabel functionalities
+    """R interface to a subset of OpenBabel functionalities
 
-	ChemmineOB provides an R interface to a subset of cheminformatics functionalities implemented by the OpelBabel C++ project. OpenBabel is an open source cheminformatics toolbox that includes utilities for structure format interconversions, descriptor calculations, compound similarity searching and more. ChemineOB aims to make a subset of these utilities available from within R. For non-developers, ChemineOB is primarily intended to be used from ChemmineR as an add-on package rather than used directly.
-	"""
-	
-	homepage = "https://github.com/girke-lab/ChemmineOB"
-	bioc = "ChemmineOB" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/ChemmineOB_1.40.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/ChemmineOB/ChemmineOB_1.40.0.tar.gz"]
+    ChemmineOB provides an R interface to a subset of cheminformatics functionalities implemented by the OpelBabel C++ project. OpenBabel is an open source cheminformatics toolbox that includes utilities for structure format interconversions, descriptor calculations, compound similarity searching and more. ChemineOB aims to make a subset of these utilities available from within R. For non-developers, ChemineOB is primarily intended to be used from ChemmineR as an add-on package rather than used directly.
+    """
 
-	version("1.40.0", md5="5bd759ca398ffb70214a8076028bf0b2")
+    homepage = "https://github.com/girke-lab/ChemmineOB"
+    bioc = "ChemmineOB"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/ChemmineOB_1.40.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/ChemmineOB/ChemmineOB_1.40.0.tar.gz",
+    ]
 
-	depends_on("r@2.15.1:", type=("build", "run"))
-	depends_on("r-biocgenerics", type=("build", "run"))
-	depends_on("r-zlibbioc", type=("build", "run"))
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-bh", type=("build", "run"))
-	depends_on("openbabel@3.0.0:", type=("build", "link", "run"))
+    version("1.40.0", md5="5bd759ca398ffb70214a8076028bf0b2")
+
+    depends_on("r@2.15.1:", type=("build", "run"))
+    depends_on("r-biocgenerics", type=("build", "run"))
+    depends_on("r-zlibbioc", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-bh", type=("build", "run"))
+    depends_on("openbabel@3.0.0:", type=("build", "link", "run"))
+
+    def setup_build_environment(self, env):
+        env.set("OPEN_BABEL_INCDIR", self.spec["openbabel"].prefix.include.openbabel3)
+        env.set("OPEN_BABEL_LIBDIR", self.spec["openbabel"].prefix.lib)
