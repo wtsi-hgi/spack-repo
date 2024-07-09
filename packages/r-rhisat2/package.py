55c0caf775cb4e7910efd773ca2e237f33bc9990
Eric Hidari <feedbackeric@gmail.com>
2024-07-09 13:07:13 +0000

    disable parallel build

--- packages/r-rhisat2/package.py
+++ packages/r-rhisat2/package.py
@@ -7,18 +7,29 @@ from spack.package import *
 
 
 class RRhisat2(RPackage):
-	"""R Wrapper for HISAT2 Aligner
+    """R Wrapper for HISAT2 Aligner
 
-	An R interface to the HISAT2 spliced short-read aligner by Kim et al. (2015). The package contains wrapper functions to create a genome index and to perform the read alignment to the generated index.
-	"""
-	
-	homepage = "https://github.com/fmicompbio/Rhisat2"
-	bioc = "Rhisat2" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Rhisat2_1.18.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/Rhisat2/Rhisat2_1.18.0.tar.gz"]
+    An R interface to the HISAT2 spliced short-read aligner by Kim et al. (2015). The package contains wrapper functions to create a genome index and to perform the read alignment to the generated index.
+    """
 
-	version("1.18.0", md5="bcfb03ad1e10fa471b46f2b214ddbb2a", url="https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Rhisat2_1.18.0.tar.gz")
+    homepage = "https://github.com/fmicompbio/Rhisat2"
+    bioc = "Rhisat2"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Rhisat2_1.18.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/Rhisat2/Rhisat2_1.18.0.tar.gz",
+    ]
 
-	depends_on("r@4.2:", type=("build", "run"))
-	depends_on("r-genomicfeatures", type=("build", "run"))
-	depends_on("r-sgseq", type=("build", "run"))
-	depends_on("r-genomicranges", type=("build", "run"))
+    version(
+        "1.18.0",
+        md5="bcfb03ad1e10fa471b46f2b214ddbb2a",
+        url="https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Rhisat2_1.18.0.tar.gz",
+    )
+
+    depends_on("r@4.2:", type=("build", "run"))
+    depends_on("r-genomicfeatures", type=("build", "run"))
+    depends_on("r-sgseq", type=("build", "run"))
+    depends_on("r-genomicranges", type=("build", "run"))
+
+    def setup_build_environment(self, env):
+        # the build does not work when conducted in parallel
+        env.set("MAKEFLAGS", "-j1")
