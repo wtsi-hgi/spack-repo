7c67168577b8eb824ef88950328367ebf0f922fd
Eric Hidari <feedbackeric@gmail.com>
2024-07-10 15:07:39 +0000

    add oci lib and inc

--- packages/r-roracle/package.py
+++ packages/r-roracle/package.py
@@ -7,16 +7,22 @@ from spack.package import *
 
 
 class RRoracle(RPackage):
-	"""OCI Based Oracle Database Interface for R
+    """OCI Based Oracle Database Interface for R
 
-	Oracle Database interface (DBI) driver for R.
-   This is a DBI-compliant Oracle driver based on the OCI.
-	"""
-	
-	homepage = "http://www.oracle.com"
-	cran = "ROracle" 
+         Oracle Database interface (DBI) driver for R.
+    This is a DBI-compliant Oracle driver based on the OCI.
+    """
 
-	version("1.3-1.1", md5="01b528d46985dcf3888fae41b2f08c8a")
+    homepage = "http://www.oracle.com"
+    cran = "ROracle"
 
-	depends_on("r-dbi@0.2.5:", type=("build", "run"))
-	depends_on("oracle-instant-client", type=("build", "link", "run"))
+    version("1.3-1.1", md5="01b528d46985dcf3888fae41b2f08c8a")
+
+    depends_on("r-dbi@0.2.5:", type=("build", "run"))
+    depends_on("oracle-instant-client", type=("build", "link", "run"))
+
+    def configure_args(self):
+        return [
+            "--with-oci-lib={}".format(self.spec["oracle-instant-client"].prefix.lib),
+            "--with-oci-inc={}".format(self.spec["oracle-instant-client"].prefix.include),
+        ]
