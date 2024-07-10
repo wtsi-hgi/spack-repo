970a0af59e71ef603c6ffd88dbf3db76515fc217
Eric Hidari <feedbackeric@gmail.com>
2024-07-10 14:06:49 +0000

    add dep

--- packages/r-simplesinglecell/package.py
+++ packages/r-simplesinglecell/package.py
@@ -7,19 +7,22 @@ from spack.package import *
 
 
 class RSimplesinglecell(RPackage):
-	"""A step-by-step workflow for low-level analysis of single-cell RNA-seq data with Bioconductor
+    """A step-by-step workflow for low-level analysis of single-cell RNA-seq data with Bioconductor
 
-	Once a proud workflow package, this is now a shell of its former self. Almost all of its content has been cannibalized for use in the "Orchestrating Single-Cell Analyses with Bioconductor" book at https://osca.bioconductor.org. Most vignettes here are retained as reminders of the glory that once was, also providing redirection for existing external links to the relevant OSCA book chapters.
-	"""
-	
-	homepage = "https://www.bioconductor.org/help/workflows/simpleSingleCell/"
-	bioc = "simpleSingleCell" 
-	urls = ["https://www.bioconductor.org/packages/3.18/workflows/src/contrib/simpleSingleCell_1.26.0.tar.gz", "https://www.bioconductor.org/packages/3.18/workflows/src/contrib/Archive/simpleSingleCell/simpleSingleCell_1.26.0.tar.gz"]
+    Once a proud workflow package, this is now a shell of its former self. Almost all of its content has been cannibalized for use in the "Orchestrating Single-Cell Analyses with Bioconductor" book at https://osca.bioconductor.org. Most vignettes here are retained as reminders of the glory that once was, also providing redirection for existing external links to the relevant OSCA book chapters.
+    """
 
-	version("1.26.0", md5="c207dcd6c9b07a01e3c2e4fefbc065c1")
+    homepage = "https://www.bioconductor.org/help/workflows/simpleSingleCell/"
+    bioc = "simpleSingleCell"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/workflows/src/contrib/simpleSingleCell_1.26.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/workflows/src/contrib/Archive/simpleSingleCell/simpleSingleCell_1.26.0.tar.gz",
+    ]
 
-	depends_on("r-knitr", type=("build", "run"))
-	depends_on("r-callr", type=("build", "run"))
-	depends_on("r-rmarkdown", type=("build", "run"))
-	depends_on("r-biocstyle", type=("build", "run"))
+    version("1.26.0", md5="c207dcd6c9b07a01e3c2e4fefbc065c1")
 
+    depends_on("r-knitr", type=("build", "run"))
+    depends_on("r-callr", type=("build", "run"))
+    depends_on("r-rmarkdown", type=("build", "run"))
+    depends_on("r-biocstyle", type=("build", "run"))
+    depends_on("r-codedepends", type=("build", "run"))
