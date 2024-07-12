012273992129b6963f823e1b5cbcc97c0e5c4d04
Michael Woolnough <mw31@sanger.ac.uk>
2024-07-12 14:30:10 +0100

    Correct dependency for inbuilt package savanna.

---
+++ packages/savanna/package.py
@@ -0,0 +1,29 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+
+class Savanna(MakefilePackage):
+    """CODARcode Savanna runtime framework for high performance,
+    workflow management using Swift/T and ADIOS.
+    """
+
+    homepage = "https://github.com/CODARcode/savanna"
+    git = "https://github.com/CODARcode/savanna.git"
+
+    version("1.0", tag="v1.0", submodules=True)
+    version("0.5", tag="v0.5", submodules=True)
+
+    variant("tau", default=False, description="Enable TAU profiling support")
+
+    depends_on("mpi")
+    depends_on("stc")
+    depends_on("adios +fortran +zlib +sz +zfp staging=dataspaces")  # flexpath
+    depends_on("turbine")
+    depends_on("tau", when="+tau")
+
+    def install(self, spec, prefix):
+        install_tree(".", prefix)
