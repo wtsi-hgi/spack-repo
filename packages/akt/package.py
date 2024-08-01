f16c05db8270dbd4d2250d21ce45cbe987d6587e
Eric Hidari <feedbackeric@gmail.com>
2024-08-01 12:54:37 +0000

    add akt package

---
+++ packages/akt/package.py
@@ -0,0 +1,45 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install akt
+#
+# You can edit this file again by typing:
+#
+#     spack edit akt
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack.package import *
+
+
+class Akt(MakefilePackage):
+    """Ancestry and Kinship Tools (AKT) provides a handful of useful statistical genetics routines using the htslib API for input/output. This means it can seamlessly read BCF/VCF files and play nicely with bcftools."""
+
+    homepage = "https://github.com/Illumina/akt"
+    url = "https://github.com/Illumina/akt/archive/refs/tags/v0.3.3.tar.gz"
+
+    license("GPL-3.0")
+
+    version("0.3.3", sha256="1b077dde944cb13132e4fb5b47d4930c1ecfc74b299c95fe3cc7bf5c17b8f710")
+    version("0.3.2", sha256="a61f479208d68939c2d557ba72d232072f1eec53c626ff078ee7f09b5cfac716")
+
+    depends_on("zlib-api")
+
+    def patch(self):
+        which("chmod")("+x", "htslib-1.6/version.sh")
+        filter_file("$(shell git describe --abbrev=4 --always )", "v{}".format(self.version), "Makefile", string=True)
+
+    def install(self, spec, prefix):
+        mkdirp(prefix.bin)
+        install("akt", prefix.bin)
