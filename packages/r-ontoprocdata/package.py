dfa5ea42b05bf076487c44563f5b658c03a83fe9
Eric Hidari <feedbackeric@gmail.com>
2024-08-12 09:09:39 +0000

    fix SHA

--- packages/r-ontoprocdata/package.py
+++ packages/r-ontoprocdata/package.py
@@ -7,15 +7,15 @@ from spack.package import *
 
 
 class ROntoprocdata(RPackage):
-	"""A data package for ontoProc
+    """A data package for ontoProc
 
-	This package manages rda files of multiple ontologies that are used in the ontoProc package. These ontologies were originally downloaded as owl or obo files and converted into Rda files. The files were downloaded at various times but most of them were downloaded on August 08 2022.
-	"""
-	
-	bioc = "ontoProcData" 
-	urls = ["https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/ontoProcData_0.99.9901.tar.gz", "https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/Archive/ontoProcData/ontoProcData_0.99.9901.tar.gz"]
+    This package manages rda files of multiple ontologies that are used in the ontoProc package. These ontologies were originally downloaded as owl or obo files and converted into Rda files. The files were downloaded at various times but most of them were downloaded on August 08 2022.
+    """
+    
+    bioc = "ontoProcData" 
+    url = "https://www.bioconductor.org/packages/release/data/annotation/src/contrib/ontoProcData_0.99.9901.tar.gz"
 
-	version("0.99.9901", md5="d4160c1fc853db468ce73c7fe62ca00f")
-
-	depends_on("r@4.2:", type=("build", "run"))
+    version("0.99.9901", sha256="33a35841b2ee616dad02e82adc49b64d71bc6895cfe1be834ba8369846819c9b")
+    
+    depends_on("r@4.2:", type=("build", "run"))
 
