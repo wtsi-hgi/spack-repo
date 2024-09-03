e3fb9fd8c41b000d19684b3d7db82d0a2829aa61
Eric Hidari <feedbackeric@gmail.com>
2024-09-03 12:22:55 +0000

    fix the version identifier

--- packages/r-qlcmatrix/package.py
+++ packages/r-qlcmatrix/package.py
@@ -17,7 +17,7 @@ Comparison
 
 	version("0.9.7", md5="3b0734cb96c9cae62f760de1d4a4a8a7")
 
-	depends_on("r-matrix@1.1-0:", type=("build", "run"))
+	depends_on("r-matrix@1.1.0:", type=("build", "run"))
 	depends_on("r@3.2:", type=("build", "run"))
 	depends_on("r-slam@0.1-32:", type=("build", "run"))
 	depends_on("r-sparsesvd", type=("build", "run"))
