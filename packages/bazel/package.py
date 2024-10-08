1fd87b91d44ebbc6172a2aca4f6e56c511769eb2
Eric Hidari <feedbackeric@gmail.com>
2024-10-08 20:59:36 +0000

    add support of later openjdk

--- packages/bazel/package.py
+++ packages/bazel/package.py
@@ -80,7 +80,7 @@ class Bazel(Package):
     )
 
     # https://bazel.build/install/compile-source#bootstrap-unix-prereq
-    depends_on("openjdk@11", when="@5.3:", type=("build", "run"))
+    depends_on("openjdk@11:", when="@5.3:", type=("build", "run"))
     depends_on("openjdk@1.8,11", when="@3.3:5.2", type=("build", "run"))
     depends_on("openjdk@1.8", when="@0.6:3.2", type=("build", "run"))
     depends_on("python+pythoncmd", type=("build", "run"))
