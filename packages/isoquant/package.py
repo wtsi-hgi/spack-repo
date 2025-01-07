e40e9f863cd8e33f67551fa4fb0933b15570254e
Eric Hidari <feedbackeric@gmail.com>
2025-01-07 14:03:37 +0000

    Update Isoquant package versions to include 3.6.2

--- packages/isoquant/package.py
+++ packages/isoquant/package.py
@@ -14,50 +14,18 @@ class Isoquant(Package):
     homepage = "https://github.com/ablab/IsoQuant"
     url = "https://github.com/ablab/IsoQuant/archive/refs/tags/v3.5.2.tar.gz"
 
-    version(
-        "3.6.1",
-        sha256="5e55749b0e7694382892929b283979bf513ee30e4c405c8ee9ddc1e40032f7dd",
-    )
-    version(
-        "3.5.2",
-        sha256="cad3d8df81324fd4a857c2ad8057c6207cb66f422f90afe36337aa95d4135951",
-    )
-    version(
-        "3.5.1",
-        sha256="1840c6a7224951bf4084f840201c42628c439c0d0404f12d47734eec403f8179",
-    )
-    version(
-        "3.5.0",
-        sha256="8cbba80b5eb0ed85fe0b519693157eb97820bc1d79ff44435736bf799af85c1f",
-    )
-    version(
-        "3.4.2",
-        sha256="7e620dc1aaffecc19155137c6766e47b3fea07822626bd2a23e7b8e57ffac18f",
-    )
-    version(
-        "3.4.1",
-        sha256="df30e662730071908c8f048037ca93821ac6c62a963aaaa9da783bf341871d60",
-    )
-    version(
-        "3.4.0",
-        sha256="19fb81cc97b3d65fecbd996e099d2a906c894de3984e5bc7b99852887696b171",
-    )
-    version(
-        "3.3.1",
-        sha256="5b6f47f46eb95254f5df1c3f1ba113361011adeada4396f680f5e456460d02f2",
-    )
-    version(
-        "3.3.0",
-        sha256="5c0e793a33f1b39b9f4888f7bade7f827f68bfb113535e08099906f7ad4689ce",
-    )
-    version(
-        "3.2.0",
-        sha256="8286b9d65bc2d755b742766ca8e0d0b86443ff2246dab6e68c213d54b4ede67c",
-    )
-    version(
-        "3.1.2",
-        sha256="7a0175cfb613485c1ea405457677eb6660a99b6aa794fd2e84fa72ed738cab92",
-    )
+    version("3.6.2", sha256="7d620cdf4055a55132d6c66b45b706172cd5aae71bc91ac48e409311e8cc1530")
+    version("3.6.1", sha256="5e55749b0e7694382892929b283979bf513ee30e4c405c8ee9ddc1e40032f7dd")
+    version("3.5.2", sha256="cad3d8df81324fd4a857c2ad8057c6207cb66f422f90afe36337aa95d4135951")
+    version("3.5.1", sha256="1840c6a7224951bf4084f840201c42628c439c0d0404f12d47734eec403f8179")
+    version("3.5.0", sha256="8cbba80b5eb0ed85fe0b519693157eb97820bc1d79ff44435736bf799af85c1f")
+    version("3.4.2", sha256="7e620dc1aaffecc19155137c6766e47b3fea07822626bd2a23e7b8e57ffac18f")
+    version("3.4.1", sha256="df30e662730071908c8f048037ca93821ac6c62a963aaaa9da783bf341871d60")
+    version("3.4.0", sha256="19fb81cc97b3d65fecbd996e099d2a906c894de3984e5bc7b99852887696b171")
+    version("3.3.1", sha256="5b6f47f46eb95254f5df1c3f1ba113361011adeada4396f680f5e456460d02f2")
+    version("3.3.0", sha256="5c0e793a33f1b39b9f4888f7bade7f827f68bfb113535e08099906f7ad4689ce")
+    version("3.2.0", sha256="8286b9d65bc2d755b742766ca8e0d0b86443ff2246dab6e68c213d54b4ede67c")
+    version("3.1.2", sha256="7a0175cfb613485c1ea405457677eb6660a99b6aa794fd2e84fa72ed738cab92")
 
     depends_on("samtools", type="run")
     depends_on("minimap2", type="run")
