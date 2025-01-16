95b8eea18c37e20e8c20a5a056c01600bebb213a
Michael Woolnough <mw31@sanger.ac.uk>
2025-01-16 15:56:54 +0000

    Add new version for py-mofapy2.

--- packages/py-mofapy2/package.py
+++ packages/py-mofapy2/package.py
@@ -11,5 +11,6 @@ class PyMofapy2(PythonPackage):
     pypi = "mofapy2/mofapy2-0.7.0.tar.gz"
 
     version("0.7.0", sha256="b89a5fc7ab68ca2a27332ca51b0882d98c2352977ca106079c5edbc82709ae95")
+    version("0.7.2", sha256="3bd7a2a7529ea7a9a91b22393079141b6e5ba5fa9c0895cdf6ff002dc26229f9")
 
     depends_on("py-poetry", type=("build", "run"))
