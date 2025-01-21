a859fd0c8c6fb840a33f019b5fc5ff5aa16fddfe
Eric Hidari <feedbackeric@gmail.com>
2025-01-21 15:24:18 +0000

    Refactor RJaspar2020 package.py: Update metadata, fix indentation, and change version hash to SHA256

--- packages/r-jaspar2020/package.py
+++ packages/r-jaspar2020/package.py
@@ -7,16 +7,18 @@ from spack.package import *
 
 
 class RJaspar2020(RPackage):
-	"""Data package for JASPAR database (version 2020)
+    """Data package for JASPAR database (version 2020)
 
-	Data package for JASPAR2020. To search this databases, please use the package TFBSTools (>= 1.23.1).
-	"""
-	
-	homepage = "http://jaspar.genereg.net/"
-	bioc = "JASPAR2020" 
-	urls = ["https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/JASPAR2020_0.99.10.tar.gz", "https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/Archive/JASPAR2020/JASPAR2020_0.99.10.tar.gz"]
+    Data package for JASPAR2020. To search this databases, please use the package TFBSTools (>= 1.23.1).
+    """
 
-	version("0.99.10", md5="bfcaf41ebf0935b8d146afd37719de2d", url="https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/JASPAR2020_0.99.10.tar.gz")
+    homepage = "http://jaspar.genereg.net/"
+    bioc = "JASPAR2020"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/JASPAR2020_0.99.10.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/data/annotation/src/contrib/Archive/JASPAR2020/JASPAR2020_0.99.10.tar.gz",
+    ]
 
-	depends_on("r@3.6:", type=("build", "run"))
+    version("0.99.10", sha256="b9b92d141a317ebb32a14708229f6b82522ceeb5f1b88360d93b0a7cfe30375b")
 
+    depends_on("r@3.6:", type=("build", "run"))
