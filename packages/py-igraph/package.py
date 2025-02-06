aa57e7a0b4f4a7dfbc88554990dd26e1f857d7c6
Eric Hidari <feedbackeric@gmail.com>
2025-02-06 14:15:03 +0000

    Add py-texttable dependency to py-igraph package
    - Include py-texttable as a build and runtime dependency
    - Maintain consistent package configuration for py-igraph

--- packages/py-igraph/package.py
+++ packages/py-igraph/package.py
@@ -3,16 +3,17 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+
 class PyIgraph(PythonPackage):
     """
     High performance graph data structures and algorithms
     """
 
     homepage = "https://igraph.org/python"
-    pypi     = "igraph/igraph-0.11.2.tar.gz"
-    
+    pypi = "igraph/igraph-0.11.2.tar.gz"
 
     version("0.11.2", sha256="c4053e47156d7685ad48bd72e6ff400d46b09deeebc746b96b5cf9737939fa2d")
 
     depends_on("py-setuptools", type="build")
     depends_on("cmake")
+    depends_on("py-texttable", type=("build", "run"))
