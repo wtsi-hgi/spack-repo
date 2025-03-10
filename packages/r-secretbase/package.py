86350275688aef04e9e6b29b3b06dd7cba129b01
Eric Hidari <feedbackeric@gmail.com>
2025-03-10 15:06:16 +0000

    Update R package versions for ps, secretbase, and targets
    - Add new versions for r-ps (1.8.0, 1.8.1, 1.9.0)
    - Update r-secretbase with versions 1.0.0 to 1.0.5
    - Bump r-targets to version 1.10.1
    - Adjust dependencies for r-targets to require specific versions of r-ps and r-secretbase

--- packages/r-secretbase/package.py
+++ packages/r-secretbase/package.py
@@ -7,9 +7,9 @@ from spack.package import *
 
 
 class RSecretbase(RPackage):
-	"""Cryptographic Hash and Extendable-Output Functions
+    """Cryptographic Hash and Extendable-Output Functions
 
-	SHA-256, SHA-3 cryptographic hash and SHAKE256 extendable-output
+    SHA-256, SHA-3 cryptographic hash and SHAKE256 extendable-output
     functions (XOF). The SHA-3 Secure Hash Standard was published by the
     National Institute of Standards and Technology (NIST) in 2015 at
     <doi:10.6028/NIST.FIPS.202>. The SHA-256 Secure Hash Standard was published
@@ -18,12 +18,19 @@ class RSecretbase(RPackage):
     memory-efficient implementation using the core algorithms from 'Mbed TLS'
     under the Trusted Firmware Project
     <https://www.trustedfirmware.org/projects/mbed-tls/>.
-	"""
-	
-	homepage = "https://shikokuchuo.net/secretbase/"
-	cran = "secretbase" 
+    """
 
-	version("0.3.0.1", md5="e6f9c238f463c7b4f850e18988167972")
-	version("0.3.0", md5="87d9c1c967bcbdcdf4c6db3541bc905d")
+    homepage = "https://shikokuchuo.net/secretbase/"
+    cran = "secretbase"
 
-	depends_on("r@3.5:", type=("build", "run"))
+    version("1.0.5", sha256="5a8d54c07c4e2a12b50d06dcfd20d778f307d8418cd3aae390b04f0d546bb091")
+    version("1.0.4", sha256="e86fdef5a57ac6b00669092ede5ac26dad27b03e19c1f72bd79128019c68c7a6")
+    version("1.0.3", sha256="c6aece3ffd0aaefb737d8d66f27495465ed40b485edee44e32fa2ccead5cb62e")
+    version("1.0.2", sha256="89bb33b7d49056f6e31ae56748caff34867eb5cd3f77e420c6cbf979c9e5d444")
+    version("1.0.1", sha256="abb9c06abb8e0657a54786aad104de336991c3e2795226bce225bba7374d6e04")
+    version("1.0.0", sha256="90a34d65ef476b839508e29fb188d2abf49eed76b29086476b8f69c3ef9dd5d2")
+    version("0.5.0", sha256="e42d5f5d50717e22bec97e072f8dcee08c267c5af990a2eeebf1d2b4c897bd13")
+    version("0.3.0.1", md5="e6f9c238f463c7b4f850e18988167972")
+    version("0.3.0", md5="87d9c1c967bcbdcdf4c6db3541bc905d")
+
+    depends_on("r@3.5:", type=("build", "run"))
--- packages/r-targets/package.py
+++ packages/r-targets/package.py
@@ -7,42 +7,45 @@ from spack.package import *
 
 
 class RTargets(RPackage):
-	"""Dynamic Function-Oriented 'Make'-Like Declarative Pipelines
+    """Dynamic Function-Oriented 'Make'-Like Declarative Pipelines
 
-	Pipeline tools coordinate the pieces of computationally
-  demanding analysis projects.
-  The 'targets' package is a 'Make'-like pipeline tool for statistics and
-  data science in R. The package skips costly runtime for tasks
-  that are already up to date,
-  orchestrates the necessary computation with implicit parallel computing,
-  and abstracts files as R objects. If all the current output matches
-  the current upstream code and data, then the whole pipeline is up
-  to date, and the results are more trustworthy than otherwise.
-  The methodology in this package
-  borrows from GNU 'Make' (2015, ISBN:978-9881443519)
-  and 'drake' (2018, <doi:10.21105/joss.00550>).
-	"""
-	
-	homepage = "https://docs.ropensci.org/targets/"
-	cran = "targets" 
+          Pipeline tools coordinate the pieces of computationally
+    demanding analysis projects.
+    The 'targets' package is a 'Make'-like pipeline tool for statistics and
+    data science in R. The package skips costly runtime for tasks
+    that are already up to date,
+    orchestrates the necessary computation with implicit parallel computing,
+    and abstracts files as R objects. If all the current output matches
+    the current upstream code and data, then the whole pipeline is up
+    to date, and the results are more trustworthy than otherwise.
+    The methodology in this package
+    borrows from GNU 'Make' (2015, ISBN:978-9881443519)
+    and 'drake' (2018, <doi:10.21105/joss.00550>).
+    """
 
-	version("1.6.0", md5="4c49e58ed1365e6c9b6b7449b40ea6ba")
-	version("1.5.1", md5="2702be9b7dc1319a257169e291eeb0c7")
+    homepage = "https://docs.ropensci.org/targets/"
+    cran = "targets"
 
-	depends_on("r@3.5:", type=("build", "run"))
-	depends_on("r-base64url@1.4:", type=("build", "run"))
-	depends_on("r-callr@3.7:", type=("build", "run"))
-	depends_on("r-cli@2.0.2:", type=("build", "run"))
-	depends_on("r-codetools@0.2.16:", type=("build", "run"))
-	depends_on("r-data-table@1.12.8:", type=("build", "run"))
-	depends_on("r-digest@0.6.25:", type=("build", "run"))
-	depends_on("r-igraph@2:", type=("build", "run"))
-	depends_on("r-knitr@1.34:", type=("build", "run"))
-	depends_on("r-ps", type=("build", "run"))
-	depends_on("r-r6@2.4.1:", type=("build", "run"))
-	depends_on("r-rlang@1:", type=("build", "run"))
-	depends_on("r-secretbase", type=("build", "run"))
-	depends_on("r-tibble@3.0.1:", type=("build", "run"))
-	depends_on("r-tidyselect@1.1:", type=("build", "run"))
-	depends_on("r-vctrs@0.2.4:", type=("build", "run"))
-	depends_on("r-yaml@2.2.1:", type=("build", "run"))
+    version("1.10.1", sha256="20966888fbbb812bef182391fa9e3f5b73dbe540a09364bb8495e970385e553a")
+    version("1.6.0", sha256="4c49e58ed1365e6c9b6b7449b40ea6ba")
+    version("1.5.1", sha256="2702be9b7dc1319a257169e291eeb0c7")
+
+    depends_on("r@3.5:", type=("build", "run"))
+    depends_on("r-base64url@1.4:", type=("build", "run"))
+    depends_on("r-callr@3.7:", type=("build", "run"))
+    depends_on("r-cli@2.0.2:", type=("build", "run"))
+    depends_on("r-codetools@0.2.16:", type=("build", "run"))
+    depends_on("r-data-table@1.12.8:", type=("build", "run"))
+    depends_on("r-digest@0.6.25:", type=("build", "run"))
+    depends_on("r-igraph@2:", type=("build", "run"))
+    depends_on("r-knitr@1.34:", type=("build", "run"))
+    depends_on("r-ps@1.8.0:", type=("build", "run"), when="@1.10.1:")
+    depends_on("r-ps", type=("build", "run"))
+    depends_on("r-r6@2.4.1:", type=("build", "run"))
+    depends_on("r-rlang@1:", type=("build", "run"))
+    depends_on("r-secretbase@0.5.0:", type=("build", "run"), when="@1.10.1:")
+    depends_on("r-secretbase", type=("build", "run"))
+    depends_on("r-tibble@3.0.1:", type=("build", "run"))
+    depends_on("r-tidyselect@1.1:", type=("build", "run"))
+    depends_on("r-vctrs@0.2.4:", type=("build", "run"))
+    depends_on("r-yaml@2.2.1:", type=("build", "run"))
