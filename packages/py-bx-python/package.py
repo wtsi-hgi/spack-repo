2c769c3c873e9bf20f6279b278964a5b7ebc3055
Eric Hidari <feedbackeric@gmail.com>
2025-03-18 16:42:37 +0000

    Update py-bx-python package to specify Python version dependencies for different versions
    - Set Python dependency to 'python@2.7:2.8,3.5:' for version 0.8.8.
    - Specify Python dependency to 'python@3.7:' for version 0.10.0.
    - Adjust zlib dependency to be applicable for all versions.

--- packages/py-bx-python/package.py
+++ packages/py-bx-python/package.py
@@ -16,10 +16,11 @@ class PyBxPython(PythonPackage):
     version("0.10.0", sha256="bfe9541d7b18a98e907b085e31f58d3989fbca4dc667c4ae48c33b753e0e2da8")
     version("0.8.8", sha256="ad0808ab19c007e8beebadc31827e0d7560ac0e935f1100fb8cc93607400bb47")
 
-    depends_on("python@2.7:2.8,3.5:", type=("build", "run"))
+    depends_on("python@2.7:2.8,3.5:", type=("build", "run"), when="@0.8.8")
+    depends_on("python@3.7:", type=("build", "run"), when="@0.10.0:")
     depends_on("py-setuptools", type="build")
     depends_on("py-cython", type="build")
     depends_on("py-numpy", type=("build", "run"))
     depends_on("py-six", type=("build", "run"))
     depends_on("lzo", type=("build", "link"), when="@0.10.0:")
-    depends_on("zlib", type=("build", "link"), when="@0.10.0:")
+    depends_on("zlib", type=("build", "link"))
