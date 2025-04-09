6dedebfa119fa416dd90ee906e6dbc418717b127
Michael Woolnough <mw31@sanger.ac.uk>
2025-04-09 10:27:12 +0100

    Add recipe for dnacomb

---
+++ packages/dnacomb/package.py
@@ -0,0 +1,22 @@
+# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack.package import *
+
+class Dnacomb(Package):
+    """CLI tool for counting structured single and paired end sequencing reads and comparing them to an expected library. It compares each read to a canonical form defined in a library specification using one of four approaches:"""
+
+    homepage = "https://github.com/allydunham/dnacomb"
+    url = "https://github.com/allydunham/dnacomb/archive/refs/tags/v0.1.0.tar.gz"
+
+    version("0.1.0", sha256="25310c46ee0da65f05de7dfc97fabd5fbfbd2646c49477897a4487d530a9dc8f")
+
+    depends_on("rust", type="build")
+
+    def install(self, spec, prefix):
+        cargo = which("cargo")
+        cargo("build", "--release")
+        mkdirp(prefix.bin)
+        install("target/release/dnacomb", prefix.bin)
