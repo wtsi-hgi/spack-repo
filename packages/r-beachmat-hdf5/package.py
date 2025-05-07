a8b5a3811ec1af0ec375ff9bb1000ee21dbaa79f
Eric Hidari <feedbackeric@gmail.com>
2025-05-07 09:18:36 +0000

    Update RBeachmatHdf5 package with new version 1.6.0 and improved dependency management.

--- packages/r-beachmat-hdf5/package.py
+++ packages/r-beachmat-hdf5/package.py
@@ -7,19 +7,28 @@ from spack.package import *
 
 
 class RBeachmatHdf5(RPackage):
-	"""beachmat bindings for HDF5-backed matrices
+    """beachmat bindings for HDF5-backed matrices
 
-	Extends beachmat to support initialization of tatami matrices from HDF5-backed arrays. This allows C++ code in downstream packages to directly call the HDF5 C/C++ library to access array data, without the need for block processing via DelayedArray. Some utilities are also provided for direct creation of an in-memory tatami matrix from a HDF5 file.
-	"""
-	
-	bioc = "beachmat.hdf5" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/beachmat.hdf5_1.0.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/beachmat.hdf5/beachmat.hdf5_1.0.0.tar.gz"]
+    Extends beachmat to support initialization of tatami matrices from HDF5-backed arrays. This allows C++ code in downstream packages to directly call the HDF5 C/C++ library to access array data, without the need for block processing via DelayedArray. Some utilities are also provided for direct creation of an in-memory tatami matrix from a HDF5 file.
+    """
 
-	version("1.0.0", md5="b2ca9b6358fb05895101e6a911ca0e39", url="https://www.bioconductor.org/packages/3.18/bioc/src/contrib/beachmat.hdf5_1.0.0.tar.gz")
+    bioc = "beachmat.hdf5"
+    urls = [
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/beachmat.hdf5_1.0.0.tar.gz",
+        "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/beachmat.hdf5/beachmat.hdf5_1.0.0.tar.gz",
+    ]
 
-	depends_on("r-beachmat", type=("build", "run"))
-	depends_on("r-hdf5array", type=("build", "run"))
-	depends_on("r-delayedarray", type=("build", "run"))
-	depends_on("r-rcpp", type=("build", "run"))
-	depends_on("r-rhdf5lib", type=("build", "run"))
-	depends_on("zlib", type=("build", "link", "run"))
+    version("1.6.0", tag="RELEASE_3_21")
+    version(
+        "1.0.0",
+        md5="b2ca9b6358fb05895101e6a911ca0e39",
+        url="https://www.bioconductor.org/packages/3.18/bioc/src/contrib/beachmat.hdf5_1.0.0.tar.gz",
+    )
+
+    depends_on("r-beachmat", type=("build", "run"))
+    depends_on("r-assorthead", type=("build", "run"), when="@1.6:")
+    depends_on("r-hdf5array", type=("build", "run"))
+    depends_on("r-delayedarray", type=("build", "run"))
+    depends_on("r-rcpp", type=("build", "run"))
+    depends_on("r-rhdf5lib", type=("build", "run"))
+    depends_on("zlib", type=("build", "link", "run"))
