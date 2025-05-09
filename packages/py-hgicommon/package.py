a2b8fb36f58e18010447a2e0a5db75b4f020e97c
Eric Hidari <feedbackeric@gmail.com>
2025-05-09 13:33:31 +0000

    Update PyBaton and PyHgicommon for version 1.3.2 dependency

--- packages/py-hgicommon/package.py
+++ packages/py-hgicommon/package.py
@@ -10,7 +10,8 @@ class PyHgicommon(PythonPackage):
 
     homepage = "https://github.com/wtsi-hgi/python-common"
     pypi = "hgicommon/hgicommon-1.2.0.tar.gz"
-
+    
+    version("1.3.2", sha256="b1c136af067508e700460412da510f725f36e50b28d3170f0c3d4fe308ea21fe")
     version("1.3.1", sha256="89a5a55160e6519d281d5bc2ba7510b47b08713ebf0a8116d545bf9dffdb7cc3")
     version("1.2.0", sha256="1bfcfe47849beaab3a4890ba0bbc29fb6de853b34a4ad09e91b365c61db6308f")
 
