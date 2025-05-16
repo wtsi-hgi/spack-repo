c4778ace1b3ecad1b8ad2b488b06abb22fb2a7bf
Eric Hidari <feedbackeric@gmail.com>
2025-05-16 11:58:43 +0000

    Update REnrichr and RHdwgcna packages: add new versions, improve descriptions, and update dependencies.

--- packages/r-enrichr/package.py
+++ packages/r-enrichr/package.py
@@ -7,18 +7,25 @@ from spack.package import *
 
 
 class REnrichr(RPackage):
-	"""Provides an R Interface to 'Enrichr'
+    """Provides an R Interface to 'Enrichr'
 
-	Provides an R interface to all 'Enrichr' databases. 'Enrichr' is a web-based tool for analysing gene sets and returns any enrichment of common annotated biological features. Quoting from their website 'Enrichment analysis is a computational method for inferring knowledge about an input gene set by comparing it to annotated gene sets representing prior biological knowledge.' See <https://maayanlab.cloud/Enrichr/> for further details.
-	"""
-	
-	cran = "enrichR" 
+    Provides an R interface to all 'Enrichr' databases. 'Enrichr' is a web-based tool for analysing gene sets and returns any enrichment of common annotated biological features. Quoting from their website 'Enrichment analysis is a computational method for inferring knowledge about an input gene set by comparing it to annotated gene sets representing prior biological knowledge.' See <https://maayanlab.cloud/Enrichr/> for further details.
+    """
 
-	version("3.2", md5="998939d80840cc81b1e45b1f78fcdb69")
+    cran = "enrichR"
 
-	depends_on("r@3:", type=("build", "run"))
-	depends_on("r-httr", type=("build", "run"))
-	depends_on("r-curl", type=("build", "run"))
-	depends_on("r-rjson", type=("build", "run"))
-	depends_on("r-ggplot2", type=("build", "run"))
-	depends_on("r-writexls", type=("build", "run"))
+    version("3.4", sha256="56b16625c2c05afe6bb63b68ec06d94e9511f1532fae194e64d624f2c338843e")
+    version("3.2", md5="998939d80840cc81b1e45b1f78fcdb69")
+
+    depends_on("r@3:", type=("build", "run"))
+    depends_on("r-httr", type=("build", "run"))
+    depends_on("r-curl", type=("build", "run"))
+    depends_on("r-rjson", type=("build", "run"))
+    depends_on("r-ggplot2", type=("build", "run"))
+    depends_on("r-writexls", type=("build", "run"))
+
+    def setup_build_environment(self, env):
+        env.set("CURL_SSL_BACKEND", "openssl")
+
+    def setup_run_environment(self, env):
+        env.set("CURL_SSL_BACKEND", "openssl")
--- packages/r-hdwgcna/package.py
+++ packages/r-hdwgcna/package.py
@@ -1,16 +1,22 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
 
+
 class RHdwgcna(RPackage):
-    """FIXME: Put a proper description of your package here."""
+    """High dimensional weighted gene co-expression network analysis (hdWGCNA)
+    for single-cell RNA-seq or spatial transcriptomics. hdWGCNA identifies
+    modules of highly co-expressed genes and provides context for these modules
+    via statistical testing and biological knowledge sources."""
 
-    homepage = "https://www.example.com"
-    url = "https://github.com/smorabit/hdWGCNA/archive/refs/tags/v0.3.00.tar.gz"
+    homepage = "https://github.com/smorabit/hdWGCNA"
+    url = "https://github.com/smorabit/hdWGCNA/archive/refs/tags/v0.3.0.tar.gz"
+    git = "https://github.com/smorabit/hdWGCNA.git"
 
+    version("0.4.05", commit="2b9df2f26339fc1e86f7223508261fb1d61001d9")
     version("0.3.00", sha256="5ef9980f6d25b41af17ebad76722766db72fb8162d5ae7af4565893d26c9ed1f")
 
     depends_on("r-harmony", type=("build", "run"))
@@ -23,3 +29,9 @@ class RHdwgcna(RPackage):
     depends_on("r-dplyr", type=("build", "run"))
     depends_on("r-tester", type=("build", "run"))
     depends_on("r-proxy", type=("build", "run"))
+    depends_on("r-ggraph", type=("build", "run"))
+    depends_on("r-tidygraph", type=("build", "run"))
+    depends_on("r-ucell", type=("build", "run"))
+    depends_on("r-geneoverlap", type=("build", "run"))
+    depends_on("r-genomicranges", type=("build", "run"))
+    depends_on("r-enrichr", type=("build", "run"))
