f366ffec9e397d14c812a5f065e43c29f3e91e70
Michael Woolnough <mw31@sanger.ac.uk>
2025-05-16 15:54:45 +0100

    Add new version for r-matrix

--- packages/r-matrix/package.py
+++ packages/r-matrix/package.py
@@ -16,6 +16,7 @@ class RMatrix(RPackage):
 
 	cran = "Matrix"
 	version("1.6-5", md5="85ad20ca9927bca63a0c030d81d9967b")
+	version("1.6-1", md5="08413767ca52dd8f81978e7fb3a40fff")
 	version("1.5-4", sha256="15ceb61993d61b442068104abb46e6d91b5a1179c01eeb64563b853abab66f06")
 	version("1.5-1", sha256="557dba0358172d67dc63eb5db90841915bb5ce1528f941a8005ae808d635575d")
 	version("1.4-1", sha256="42b24f1d1e94482b0ff0ef1292e2df29f69694bdbee47b3d6bfeec46fafb2f7e")
