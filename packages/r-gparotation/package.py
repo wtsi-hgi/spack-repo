7e48374cb28bb26095c27a6cb973afc51bdedd7b
Michael Woolnough <mw31@sanger.ac.uk>
2025-05-16 15:24:14 +0100

    Add new version for r-gparotation

--- packages/r-gparotation/package.py
+++ packages/r-gparotation/package.py
@@ -15,6 +15,7 @@ class RGparotation(RPackage):
 	cran = "GPArotation"
 	version("2024.3-1", md5="bcb6ace2512c0c9a3019e2160a76cf64")
 	version("2024.2-1", md5="2b2d20d78042c6679be1f86fcd72de59")
+	version("2023.8-1", sha256="d4a15a0cb3438a5ff1f80621d5a3f764")
 	version("2023.3-1", sha256="8748086c3d45286b7c9a81f0f8e58df75a09ba555d48a6eb8cd94af0c7c92a26")
 	version("2022.4-1", sha256="231e7edcdcc091fbecfb4f2e88d1a4344967cf7ea58074b385a4b8b48d9da224")
 	version("2022.10-2", sha256="04f72d8f3a9c204df5df904be563ec272a8437a707daee8823b2a690dde21917")
