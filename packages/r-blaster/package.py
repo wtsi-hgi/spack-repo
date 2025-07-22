1b96230e25387422b77f50c44b6d8f78aed0121a
mercury <mercury@sanger.ac.uk>
2025-07-22 11:14:00 +0000

    Fix 4 R package errors: install missing dependencies and update checksums

--- packages/r-blaster/package.py
+++ packages/r-blaster/package.py
@@ -18,6 +18,10 @@ class RBlaster(RPackage):
 	homepage = "https://github.com/tamminenlab/blaster"
 	cran = "blaster" 
 
-	version("1.0.7", md5="80ecfcdc4cbd001e2db5f8a622b96eed")
+	version("1.0.7", sha256="6079eefe8afd2184eec81cc34fa8c8c34703a09ce35292cd6c730efb9b390ddc")
+	version("1.0.6", sha256="04632503c43bafc21875069825b45da1f57ee2fc7f2cbc0b168701cb75400429")
+	version("1.0.4", sha256="59c257c8303da8e505b05ba0b840d56020549c2fbef3e3367eb6075ff56c5b78")
+	version("1.0.3", sha256="410d5900060914d2dbb821f798639a09a9ad20bfe49e5be5d08efef08b1c0032")
+	version("1.0", sha256="a77599683cd2b4f6e77df4b12dc42a72731a7b6a3c9ef84e804b85655544f505")
 
 	depends_on("r-rcpp", type=("build", "run"))
