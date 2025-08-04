f37d9cdd074c8727fdf9c4fc88a6856b28116072
mercury <mercury@sanger.ac.uk>
2025-08-04 08:41:01 +0000

    Modify dnacomb recipe

--- packages/dnacomb/package.py
+++ packages/dnacomb/package.py
@@ -9,9 +9,9 @@ class Dnacomb(Package):
     """CLI tool for counting structured single and paired end sequencing reads and comparing them to an expected library. It compares each read to a canonical form defined in a library specification using one of four approaches:"""
 
     homepage = "https://github.com/allydunham/dnacomb"
-    url = "https://github.com/allydunham/dnacomb/archive/refs/tags/v0.1.0.tar.gz"
+    url = "https://github.com/allydunham/dnacomb/archive/refs/tags/v0.2.0.tar.gz"
 
-    version("0.1.0", sha256="25310c46ee0da65f05de7dfc97fabd5fbfbd2646c49477897a4487d530a9dc8f")
+    version("0.2.0", sha256="63e75e9f099c0975818b0327d0019761b60c73fe142ba68e437c7caa6a6eb526")
 
     depends_on("rust", type="build")
 
