0de21b0c618d8f1936a97386756770c9e04711fa
mercury <mercury@sanger.ac.uk>
2025-08-08 16:00:15 +0000

    py-cutesv: add 2.1.2; fix py-cigar build dep

--- packages/py-cutesv/package.py
+++ packages/py-cutesv/package.py
@@ -9,8 +9,9 @@ class PyCutesv(PythonPackage):
     """Long-read sequencing enables the comprehensive discovery of structural variations (SVs). However, it is still non-trivial to achieve high sensitivity and performance simultaneously due to the complex SV characteristics implied by noisy long reads. Therefore, we propose cuteSV, a sensitive, fast and scalable long-read-based SV detection approach. cuteSV uses tailored methods to collect the signatures of various types of SVs and employs a clustering-and-refinement method to analyze the signatures to implement sensitive SV detection. Benchmarks on real Pacific Biosciences (PacBio) and Oxford Nanopore Technology (ONT) datasets demonstrate that cuteSV has better yields and scalability than state-of-the-art tools."""
 
     homepage = "https://github.com/tjiangHIT/cuteSV"
-    pypi = "cuteSV/cuteSV-2.1.1.tar.gz"
+    pypi = "cuteSV/cuteSV-2.1.2.tar.gz"
 
+    version("2.1.2", sha256="f23a31c06e11496dd3672a64a91aa57d3c51fae546909ec6485f65358cf4c57a")
     version("2.1.1", sha256="af4c50a3d0091275a09e50a025047fdf1afc7f8df5c98cf9ab27ea0ea559bc70")
 
     depends_on("python@3:", type=("build", "run"))
