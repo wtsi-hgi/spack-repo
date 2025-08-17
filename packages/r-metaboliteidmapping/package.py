926efd7f7911a608c408fdd243a95edf26ceefe9
mercury <mercury@sanger.ac.uk>
2025-08-17 10:39:53 +0000

    fix(r-metaboliteidmapping): provide valid Bioconductor source URL for 1.0.0 to unblock r-multigsea install on R>=4.5
    Spack failed fetching RMetaboliteidmapping due to missing URL. Point 1.0.0 to Bioconductor data/annotation tarball and keep a general url_for_version for future releases.

--- packages/r-metaboliteidmapping/package.py
+++ packages/r-metaboliteidmapping/package.py
@@ -19,7 +19,18 @@ class RMetaboliteidmapping(RPackage):
     version(
         "1.0.0",
         sha256="148fd847cf4609086d1e9a175712645182d9a7e4bc28a99e88c8f2a1e8bed459",
+        url="https://www.bioconductor.org/packages/3.21/data/annotation/src/contrib/metaboliteIDmapping_1.0.0.tar.gz",
     )
 
     depends_on("r@4:", type=("build", "run"))
     depends_on("r-annotationhub", type=("build", "run"))
+
+    def url_for_version(self, version):
+        """Construct Bioconductor source URL for this package.
+
+        Use the release path; older versions will be served from Archive by
+        Bioconductor's redirect logic.
+        """
+        return "https://bioconductor.org/packages/release/bioc/src/contrib/{0}_{1}.tar.gz".format(
+            self.bioc, version
+        )
