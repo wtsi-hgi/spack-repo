48cdaa0ffd42ed2ebc89cda1a03e5668832b450a
mercury <mercury@sanger.ac.uk>
2025-09-03 07:08:10 +0000

    r-dndscv: refresh recipe, add license/variants, update to 20250515\n\n- Switch homepage to GitHub; use .git URL\n- Add GPL-3.0-or-later license\n- Add default R dependencies from DESCRIPTION\n- Add optional knitr/rmarkdown variants\n- Replace dated commit with commit-based version 20250515

--- packages/r-dndscv/package.py
+++ packages/r-dndscv/package.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
+# Copyright 2013-2025 Lawrence Livermore National Security, LLC and other
 # Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
@@ -7,18 +7,37 @@ from spack.package import *
 
 
 class RDndscv(RPackage):
-    """The dNdScv R package is a group of maximum-likelihood dN/dS methods designed to quantify selection in cancer and somatic evolution (Martincorena et al., 2017)."""
+    """Poisson-based dN/dS models to quantify natural selection in somatic
+    evolution. Provides functions for studying selection on coding sequences
+    using a Poisson implementation of dN/dS, including context-dependent
+    mutation effects and selection on nonsense and splice site mutations."""
 
-    homepage = "https://www.sanger.ac.uk/tool/dndscv/"
-    git = "https://github.com/im3sanger/dndscv"
+    homepage = "https://github.com/im3sanger/dndscv"
+    git = "https://github.com/im3sanger/dndscv.git"
 
-    version("2023-09-29", commit="ae10c765ecb6b3a6ff06cd5a50aadc317dab2289")
+    license("GPL-3.0-or-later")
 
-    depends_on("r-seqinr", type=("build", "run"))
-    depends_on("r-mass", type=("build", "run"))
-    depends_on("r-genomicranges", type=("build", "run"))
-    depends_on("r-biostrings", type=("build", "run"))
-    depends_on("r-iranges", type=("build", "run"))
-    depends_on("r-rsamtools", type=("build", "run"))
-    depends_on("r-poilog", type=("build", "run"))
-    depends_on("r-plyr", type=("build", "run"))
+    # Latest commit-based version (use commit date as version per guidelines)
+    version("20250515", commit="69007c2bbd2d6dae003a30dcfe5dda3df722b2f8")
+
+    # Base R requirement (DESCRIPTION does not specify an R version)
+    depends_on("r", type=("build", "run"))
+
+    # Required imports from DESCRIPTION
+    with default_args(type=("build", "run")):
+        depends_on("r-seqinr")
+        depends_on("r-mass")
+        depends_on("r-genomicranges")
+        depends_on("r-biostrings")
+        depends_on("r-iranges")
+        depends_on("r-rsamtools")
+        depends_on("r-poilog")
+        depends_on("r-plyr")
+
+    # Optional suggests as variants (disabled by default)
+    variant("knitr", default=False, description="Enable vignette building with knitr")
+    variant("rmarkdown", default=False, description="Enable vignette building with rmarkdown")
+
+    depends_on("r-knitr", when="+knitr", type=("build", "run"))
+    depends_on("r-rmarkdown", when="+rmarkdown", type=("build", "run"))
+
