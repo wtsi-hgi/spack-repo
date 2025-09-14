9e487ba09ed239b994c76331e202a57ebf1e8b70
mercury <mercury@sanger.ac.uk>
2025-09-14 18:42:15 +0000

    [auto] finalize remaining changes

--- packages/r-cyto-explore-r/package.py
+++ packages/r-cyto-explore-r/package.py
@@ -1,24 +1,5 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install r-cyto-explore-r
-#
-# You can edit this file again by typing:
-#
-#     spack edit r-cyto-explore-r
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
 
 from spack.package import *
 
@@ -27,73 +8,43 @@ class RCytoExploreR(RPackage):
     """Interactive Analysis of Cytometry Data.
 
     CytoExploreR provides an intuitive and interactive approach to analysing
-    cytometry data in R, integrating conventional and cutting-edge tools under
-    a unified, open-source framework.
+    cytometry data in R, integrating conventional and modern analysis tools
+    within a unified open-source framework.
     """
 
     homepage = "https://github.com/DillonHammill/CytoExploreR"
     git = "https://github.com/DillonHammill/CytoExploreR.git"
 
-    maintainers("softpack-bot")
-
     license("GPL-2.0-only")
 
-    # Legacy date-based snapshot
-    version("20230228", commit="0efb1cc19fc701ae03905cf1b8484c1dfeb387df")
-    # Newer 2.x line (was in r-cytoexplorer)
-    version("2.0.15", commit="69088a6c5955afb2a2576c63e162de11ded7a882", preferred=True)
+    # No formal releases; track the latest known good commit.
+    version("20250320", commit="71a8da6fabd2843ad8f35b30b455e4fcb95a3147")
 
-    depends_on("r@3.5.0:", type=("build", "run"), when="@2.0.15")
+    # R version
+    depends_on("r@3.5.0:", type=("build", "run"))
 
-    # Depends / Imports from DESCRIPTION for 2.x
+    # Core dependencies (mix of CRAN and Bioconductor packages)
     with default_args(type=("build", "run")):
-        depends_on("r-flowcore@2.3:", when="@2.0.15")
-        depends_on("r-flowworkspace@4.3.1:", when="@2.0.15")
-        depends_on("r-opencyto@1.25.2:", when="@2.0.15")
-
-        depends_on("r-biocgenerics", when="@2.0.15")
-        depends_on("r-bslib", when="@2.0.15")
-        depends_on("r-data-table", when="@2.0.15")
-        depends_on("r-dataeditr@0.1.2:", when="@2.0.15")
-        depends_on("r-dplyr", when="@2.0.15")
-        depends_on("r-future", when="@2.0.15")
-        depends_on("r-future-apply", when="@2.0.15")
-        depends_on("r-kernsmooth", when="@2.0.15")
-        depends_on("r-mass", when="@2.0.15")
-        depends_on("r-progress", when="@2.0.15")
-        depends_on("r-purrr", when="@2.0.15")
-        depends_on("r-heatmapr", when="@2.0.15")
-        depends_on("r-reticulate", when="@2.0.15")
-        depends_on("r-robustbase", when="@2.0.15")
-        depends_on("r-rhandsontable@0.3.8:", when="@2.0.15")
-        depends_on("r-shiny", when="@2.0.15")
-        depends_on("r-tidyr", when="@2.0.15")
-        depends_on("r-visnetwork", when="@2.0.15")
-        depends_on("r-xml", when="@2.0.15")
-        depends_on("r-rcpp", when="@2.0.15")  # Also in LinkingTo
-
-    # Dependencies for the legacy snapshot release
-    depends_on("r-flowcore", type=("build", "run"), when="@20230228")
-    depends_on("r-flowworkspace", type=("build", "run"), when="@20230228")
-    depends_on("r-opencyto", type=("build", "run"), when="@20230228")
-    depends_on("r-biocgenerics", type=("build", "run"), when="@20230228")
-    depends_on("r-bslib", type=("build", "run"), when="@20230228")
-    depends_on("r-data-table", type=("build", "run"), when="@20230228")
-    depends_on("r-dplyr", type=("build", "run"), when="@20230228")
-    depends_on("r-embedsom", type=("build", "run"), when="@20230228")
-    depends_on("r-flowai", type=("build", "run"), when="@20230228")
-    depends_on("r-gtools", type=("build", "run"), when="@20230228")
-    depends_on("r-mass", type=("build", "run"), when="@20230228")
-    depends_on("r-magrittr", type=("build", "run"), when="@20230228")
-    depends_on("r-purrr", type=("build", "run"), when="@20230228")
-    depends_on("r-robustbase", type=("build", "run"), when="@20230228")
-    depends_on("r-rhandsontable", type=("build", "run"), when="@20230228")
-    depends_on("r-rtsne", type=("build", "run"), when="@20230228")
-    depends_on("r-rsvd", type=("build", "run"), when="@20230228")
-    depends_on("r-shiny", type=("build", "run"), when="@20230228")
-    depends_on("r-superheat", type=("build", "run"), when="@20230228")
-    depends_on("r-tibble", type=("build", "run"), when="@20230228")
-    depends_on("r-tidyr", type=("build", "run"), when="@20230228")
-    depends_on("r-umap", type=("build", "run"), when="@20230228")
-    depends_on("r-visnetwork", type=("build", "run"), when="@20230228")
-    depends_on("r-cyto-explore-r-data", type=("build", "run"), when="@20230228")
+        depends_on("r-biocgenerics")
+        depends_on("r-bslib")
+        depends_on("r-data-table")
+        depends_on("r-dplyr")
+        depends_on("r-embedsom")
+        depends_on("r-flowai")
+        depends_on("r-flowcore@1.53.8:")
+        depends_on("r-flowworkspace@3.35.8:")
+        depends_on("r-gtools")
+        depends_on("r-magrittr")
+        depends_on("r-mass")
+        depends_on("r-opencyto@1.25.2:")
+        depends_on("r-purrr")
+        depends_on("r-robustbase")
+        depends_on("r-rhandsontable@0.3.7:")
+        depends_on("r-rsvd")
+        depends_on("r-rtsne")
+        depends_on("r-shiny")
+        depends_on("r-superheat@1.0.0:")
+        depends_on("r-tibble")
+        depends_on("r-tidyr")
+        depends_on("r-umap")
+        depends_on("r-visnetwork")
--- packages/r-superheat/package.py
+++ packages/r-superheat/package.py
@@ -1,26 +1,34 @@
-# Copyright 2013-2023 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
-#
+# Copyright 2013-2024 Lawrence Livermore National Security, LLC
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack.package import *
 
 
 class RSuperheat(RPackage):
-	"""A Graphical Tool for Exploring Complex Datasets Using Heatmaps
+    """A Graphical Tool for Exploring Complex Datasets Using Heatmaps.
 
-	A system for generating extendable and customizable heatmaps for exploring complex datasets, including big data and data with multiple data types.
-	"""
-	
-	cran = "superheat" 
+    superheat generates extendable and customizable heatmaps for exploring
+    complex datasets, including big data and data with multiple data types.
+    """
 
-	version("0.1.0", md5="c4b19907e318c4bf31655da69a476919")
+    homepage = "https://github.com/rlbarter/superheat"
+    git = "https://github.com/rlbarter/superheat.git"
 
-	depends_on("r@3.1:", type=("build", "run"))
-	depends_on("r-dplyr@0.4.3:", type=("build", "run"))
-	depends_on("r-ggplot2@2:", type=("build", "run"))
-	depends_on("r-gtable@0.1.2:", type=("build", "run"))
-	depends_on("r-magrittr@1.5:", type=("build", "run"))
-	depends_on("r-plyr@1.8.3:", type=("build", "run"))
-	depends_on("r-scales@0.3:", type=("build", "run"))
-	depends_on("r-ggdendro", type=("build", "run"))
+    license("CC0-1.0")
+
+    # No releases on CRAN for >=1.0.0; track GitHub commit.
+    version("20200907", commit="2c619578c2ae8bc46e1383183276f98b66b44a53")
+
+    depends_on("r@3.1:", type=("build", "run"))
+
+    with default_args(type=("build", "run")):
+        depends_on("r-dplyr@0.4.3:")
+        depends_on("r-ggplot2@2.0.0:")
+        depends_on("r-gtable@0.1.2:")
+        depends_on("r-magrittr@1.5:")
+        depends_on("r-plyr@1.8.3:")
+        depends_on("r-scales@0.3.0:")
+        depends_on("r-stringr@1.2.0:")
+        depends_on("r-ggdendro")
+        depends_on("r-forcats")
+
