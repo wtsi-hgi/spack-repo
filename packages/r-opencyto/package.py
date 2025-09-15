2c706f7f23f3ef026080d097d34be3c042b04f46
mercury <mercury@sanger.ac.uk>
2025-09-15 08:35:15 +0000

    r-opencyto: add git dev version 20250826 (fixes CytoExploreR gating minEvents type error); prefer dev by default

--- packages/r-opencyto/package.py
+++ packages/r-opencyto/package.py
@@ -12,10 +12,18 @@ class ROpencyto(RPackage):
 	This package is designed to facilitate the automated gating methods in sequential way to mimic the manual gating strategy.
 	"""
 	
-	bioc = "openCyto" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/openCyto_2.14.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/openCyto/openCyto_2.14.0.tar.gz"]
+	bioc = "openCyto"
+	# Add git for latest development fixes referenced by CytoExploreR issues
+	git = "https://github.com/RGLab/openCyto.git"
+	urls = [
+	    "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/openCyto_2.14.0.tar.gz",
+	    "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/openCyto/openCyto_2.14.0.tar.gz",
+	]
 
+	# Bioconductor release
 	version("2.14.0", md5="04a62bb1960f008fc218686296a3118d")
+	# Latest development commit (fixes gating minEvents type issue)
+	version("20250826", commit="8bdcadeeec798931f539809c59070f9863fefb7b", preferred=True)
 
 	depends_on("r@3.5:", type=("build", "run"))
 	depends_on("r-biobase", type=("build", "run"))
