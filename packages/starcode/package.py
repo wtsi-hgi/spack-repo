024e6120da80927feac5f6ed2dd0b30cffa411a1
mercury <mercury@sanger.ac.uk>
2025-09-29 09:55:44 +0000

    starcode: add version 1.3 and basic CLI install test; validated install (hash l4jgxjl)

--- packages/starcode/package.py
+++ packages/starcode/package.py
@@ -13,6 +13,7 @@ class Starcode(Package):
     git = "https://github.com/gui11aume/starcode"
 
     version("1.4", tag="1.4")
+    version("1.3", tag="1.3")
 
     depends_on("gcc@9.4.0", type=("build", "link"))
 
@@ -20,3 +21,9 @@ class Starcode(Package):
         make()
         mkdir(prefix + "/bin")
         install("starcode", prefix + "/bin")
+
+    @run_after("install")
+    def install_test(self):
+        with working_dir("spack-test", create=True):
+            starcode = Executable(join_path(self.prefix.bin, "starcode"))
+            starcode("--help")
