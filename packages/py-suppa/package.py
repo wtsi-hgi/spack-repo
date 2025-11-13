db31bb66c0a8d6931ca239a9a08f8583f6868182
Michael Woolnough <130465766+mjkw31@users.noreply.github.com>
2025-11-13 14:40:19 +0000

    Add py-suppa@2.4 + fix py-suppa@2.2 (#140)

--- packages/py-suppa/package.py
+++ packages/py-suppa/package.py
@@ -15,13 +15,14 @@ class PySuppa(PythonPackage):
     # Upstream license declared as MIT in setup.py
     license("MIT")
 
+    version("2.4", sha256="a71b3017dd13ceb4b8b837dfae201eb3bc55fb11dfcb201434f6ff1e4733093c", url="https://github.com/comprna/SUPPA/archive/refs/tags/v2.4.tar.gz")
+    version("2.3", sha256="c7246b0913f627067b714a38f4ec6ea7607404c7baf992c6b9e932c1c91ef738")
     version("2.2", sha256="7df88a1eeb8f9551b9599678b5958fac72da352b6e17bf1ed084b01c901a076c")
     version("2.2.1", sha256="c09253414d4c3c6e14b65b839030a87f73009b069d7b14b443f79b54e6737dab")
     version("2.2.2", sha256="d5e37df908bf09c28d324e0a8ed70a03604bde5a52966202fdfaab825b3ad243")
-    version("2.3", sha256="c7246b0913f627067b714a38f4ec6ea7607404c7baf992c6b9e932c1c91ef738")
 
     # Build system
-    depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools", type="build", when="@:2.4")
     depends_on("python@3.5:", type=("build", "run"))
 
     # Runtime dependencies from upstream setup.py install_requires
@@ -30,6 +31,11 @@ class PySuppa(PythonPackage):
     depends_on("py-pandas@0.18.0:", type=("build", "run"))
     depends_on("py-statsmodels@0.6.1:", type=("build", "run"))
     depends_on("py-scikit-learn@0.16.1:", type=("build", "run"))
+    depends_on("perl", type="run")
+
+    @when("@2.4")
+    def install(self, _a, _b):
+        self.install_cli()
 
     @run_after("install")
     def install_cli(self):
@@ -37,10 +43,17 @@ class PySuppa(PythonPackage):
         # Copy them under libexec and provide a wrapper in bin.
         src = self.stage.source_path
         libexec_dir = join_path(self.prefix.libexec, "suppa")
+
+        if self.spec.satisfies("@:2.3"):
+            src = join_path(src, "build", "lib", "SUPPA")
+
+        if self.spec.satisfies("@2.4"):
+            install_tree(join_path(src, "lib"), join_path(libexec_dir, "lib"))
+            install_tree(join_path(src, "scripts"), join_path(libexec_dir, "scripts"))
+
         mkdirp(libexec_dir)
 
-        # Copy CLI script and its module dependencies
-        for fname in (
+        pyexes = (
             "suppa.py",
             "eventGenerator.py",
             "eventClusterer.py",
@@ -48,7 +61,16 @@ class PySuppa(PythonPackage):
             "psiPerGene.py",
             "psiCalculator.py",
             "significanceCalculator.py",
-        ):
+        )
+
+        perlexes = (
+            "rmats_to_suppa_ids_SE_events.pl",
+            "rmats_to_suppa_ids_A5_events.pl",
+            "rmats_to_suppa_ids_A3_events.pl",
+        )
+
+        # Copy CLI script and its module dependencies
+        for fname in pyexes + perlexes:
             install(join_path(src, fname), libexec_dir)
 
         # Copy the package directory used by the modules
@@ -56,13 +78,23 @@ class PySuppa(PythonPackage):
 
         # Write a thin wrapper to invoke the CLI with an augmented PYTHONPATH
         mkdirp(self.prefix.bin)
-        wrapper = join_path(self.prefix.bin, "suppa.py")
-        with open(wrapper, "w") as f:
-            f.write("#!/bin/bash\n")
-            f.write("export PYTHONPATH=\"{0}:$PYTHONPATH\"\n".format(libexec_dir))
-            f.write("exec python \"{0}\" \"$@\"\n".format(join_path(libexec_dir, "suppa.py")))
-        chmod = which("chmod")
-        chmod("+x", wrapper)
+
+        for fname in pyexes:
+            wrapper = join_path(self.prefix.bin, fname)
+            with open(wrapper, "w") as f:
+                f.write("#!/bin/bash\n")
+                f.write("export PYTHONPATH=\"{0}:$PYTHONPATH\"\n".format(libexec_dir))
+                f.write("exec python \"{0}\" \"$@\"\n".format(join_path(libexec_dir, fname)))
+            chmod = which("chmod")
+            chmod("+x", wrapper)
+
+        for fname in perlexes:
+            wrapper = join_path(self.prefix.bin, fname)
+            with open(wrapper, "w") as f:
+                f.write("#!/bin/bash\n")
+                f.write("exec perl \"{0}\" \"$@\"\n".format(join_path(libexec_dir, fname)))
+            chmod = which("chmod")
+            chmod("+x", wrapper)
 
     @run_after("install")
     def install_test(self):
