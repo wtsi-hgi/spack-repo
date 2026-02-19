59ddbf74f5e24eadb26700cf77e9af36d1b49668
mercury <mercury@sanger.ac.uk>
2026-02-19 16:33:19 +0000

    Add gcc dependency

--- packages/py-af3score/package.py
+++ packages/py-af3score/package.py
@@ -42,6 +42,7 @@ class PyAf3score(PythonPackage):
     depends_on("py-zstandard", type=("build", "run"))
     depends_on("zlib", type=("build", "link"))
     depends_on("boost@1.77.0 cxxstd=14")
+    depends_on("gcc@4:")
 
 
     @run_before("install")
