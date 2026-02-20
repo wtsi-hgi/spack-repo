237a87fb4a391b535f5d88e1fb7f7268c4a7d333
mercury <mercury@sanger.ac.uk>
2026-02-20 09:51:26 +0000

    Add numpy dependency

--- packages/py-af3score/package.py
+++ packages/py-af3score/package.py
@@ -41,7 +41,7 @@ class PyAf3score(PythonPackage):
     depends_on("py-typeguard@2.13.3:", type=("build", "run"))
     depends_on("py-zstandard", type=("build", "run"))
     depends_on("zlib", type=("build", "link"))
-    depends_on("boost@1.77.0 +python cxxstd=11")
+    depends_on("boost@1.77.0 +python +numpy cxxstd=17")
 
 
     @run_before("install")
