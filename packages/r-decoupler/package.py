e25235fab42a36d7d4e85acf48c88b1522dd1cf8
mercury <mercury@sanger.ac.uk>
2026-03-17 13:59:22 +0000

    Update r-decoupler to 2.9.7

--- packages/r-decoupler/package.py
+++ packages/r-decoupler/package.py
@@ -7,26 +7,35 @@ from spack.package import *
 
 
 class RDecoupler(RPackage):
-	"""decoupleR: Ensemble of computational methods to infer biological activities from omics data
+    """Ensemble methods to infer biological activities from omics data."""
 
-	Many methods allow us to extract biological activities from omics data using information from prior knowledge resources, reducing the dimensionality for increased statistical power and better interpretability. Here, we present decoupleR, a Bioconductor package containing different statistical methods to extract these signatures within a unified framework. decoupleR allows the user to flexibly test any method with any resource. It incorporates methods that take into account the sign and weight of network interactions. decoupleR can be used with any omic, as long as its features can be linked to a biological process based on prior knowledge. For example, in transcriptomics gene sets regulated by a transcription factor, or in phospho-proteomics phosphosites that are targeted by a kinase.
-	"""
-	
-	homepage = "https://saezlab.github.io/decoupleR/"
-	bioc = "decoupleR" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/decoupleR_2.8.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/decoupleR/decoupleR_2.8.0.tar.gz"]
+    homepage = "https://bioconductor.org/packages/release/bioc/html/decoupleR.html"
+    bioc = "decoupleR"
 
-	version("2.8.0", md5="de2a391693476223ec54521aa77ba121")
+    version(
+        "2.9.7",
+        sha256="a4adc855862b31268fbff9f7a3e5e13ec9ad416cc17dd43a5d72c46492fddb5d",
+        url="https://github.com/saezlab/decoupleR/archive/refs/tags/v2.9.7.tar.gz",
+    )
+    version(
+        "2.8.0",
+        sha256="09b6d3398eec6e5aa6eacd41c8d025488e6acc771e04e43040cb8ea1feccd4ee",
+        url="https://www.bioconductor.org/packages/3.18/bioc/src/contrib/decoupleR_2.8.0.tar.gz",
+    )
 
-	depends_on("r@4:", type=("build", "run"))
-	depends_on("r-broom", type=("build", "run"))
-	depends_on("r-dplyr", type=("build", "run"))
-	depends_on("r-magrittr", type=("build", "run"))
-	depends_on("r-matrix", type=("build", "run"))
-	depends_on("r-purrr", type=("build", "run"))
-	depends_on("r-rlang", type=("build", "run"))
-	depends_on("r-stringr", type=("build", "run"))
-	depends_on("r-tibble", type=("build", "run"))
-	depends_on("r-tidyr", type=("build", "run"))
-	depends_on("r-tidyselect", type=("build", "run"))
-	depends_on("r-withr", type=("build", "run"))
+    depends_on("r@4.0:", type=("build", "run"))
+
+    with default_args(type=("build", "run")):
+        depends_on("r-biocparallel")
+        depends_on("r-broom")
+        depends_on("r-dplyr")
+        depends_on("r-magrittr")
+        depends_on("r-matrix")
+        depends_on("r-parallelly")
+        depends_on("r-purrr")
+        depends_on("r-rlang")
+        depends_on("r-stringr")
+        depends_on("r-tibble")
+        depends_on("r-tidyr")
+        depends_on("r-tidyselect")
+        depends_on("r-withr")
