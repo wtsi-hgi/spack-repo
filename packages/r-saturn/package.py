cac33e53bd6856fd8fd011d01496911e0b275d5a
mercury <mercury@sanger.ac.uk>
2026-04-28 10:08:42 +0000

    Add/update r-saturn Bioconductor recipe

--- packages/r-saturn/package.py
+++ packages/r-saturn/package.py
@@ -12,10 +12,14 @@ class RSaturn(RPackage):
 	satuRn provides a higly performant and scalable framework for performing differential transcript usage analyses. The package consists of three main functions. The first function, fitDTU, fits quasi-binomial generalized linear models that model transcript usage in different groups of interest. The second function, testDTU, tests for differential usage of transcripts between groups of interest. Finally, plotDTU visualizes the usage profiles of transcripts in groups of interest.
 	"""
 	
-	homepage = "https://github.com/statOmics/satuRn"
-	bioc = "satuRn" 
-	urls = ["https://www.bioconductor.org/packages/3.18/bioc/src/contrib/satuRn_1.10.0.tar.gz", "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/satuRn/satuRn_1.10.0.tar.gz"]
+	homepage = "https://statomics.github.io/satuRn"
+	bioc = "satuRn"
 
+	version(
+		"1.18.0",
+		sha256="05c488ebf454ebeb3679f900239950ec324bf166871f8a46435562bbeea7de75",
+		url="https://bioconductor.org/packages/release/bioc/src/contrib/satuRn_1.18.0.tar.gz",
+	)
 	version("1.10.0", md5="a0826944ab0aee6ff42127fa7eebf43c")
 
 	depends_on("r@4.1:", type=("build", "run"))
@@ -27,3 +31,8 @@ class RSaturn(RPackage):
 	depends_on("r-ggplot2", type=("build", "run"))
 	depends_on("r-boot", type=("build", "run"))
 	depends_on("r-matrix", type=("build", "run"))
+
+	@run_after("install")
+	def install_test(self):
+		r = which("Rscript")
+		r("-e", "library(satuRn)")
