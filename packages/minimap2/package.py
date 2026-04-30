7821314b78c247aadd3a23eb5f9f07314b78d58f
Octavio Villas <ov3@sanger.ac.uk>
2026-04-30 14:04:43 +0000

    Update minimap2 and fix py-sqanti3

---
+++ packages/minimap2/package.py
@@ -0,0 +1,64 @@
+# Copyright Spack Project Developers. See COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+import glob
+
+from spack.package import *
+
+
+class Minimap2(PythonPackage):
+    """Minimap2 is a versatile sequence alignment program that aligns DNA or
+    mRNA sequences against a large reference database.
+    Mappy provides a convenient interface to minimap2."""
+
+    homepage = "https://github.com/lh3/minimap2"
+    url = "https://github.com/lh3/minimap2/releases/download/v2.2/minimap2-2.2.tar.bz2"
+    git = "https://github.com/lh3/minimap2.git"
+
+    maintainers("snehring")
+
+    license("MIT")
+
+    version("2.30", sha256="f21b5fc2f8fcd02b88bf0ab9a76ad968d59dc67ac384af31ab074393d9e2c1f0")
+    version("2.29", sha256="8a231af5766b62cb9e24720290a730809633a6ea4cb7fe49ce39884438eceddb")
+    version("2.28", sha256="ffa5712735d229119f8c05722a0638ae0cc15aeb8938e29a3e52d5da5c92a0b4")
+    version("2.26", sha256="6a588efbd273bff4f4808d5190957c50272833d2daeb4407ccf4c1b78143624c")
+    version("2.24", sha256="9dd4c31ff082182948944bcdad6d328f64f09295d10547d72eba24189880a615")
+    version("2.17", sha256="b68ac8882d33cc63e9e3246775062aeb159b6990ff7f38099172c3fe6f8a2742")
+    version("2.14", sha256="9088b785bb0c33488ca3a27c8994648ce21a8be54cb117f5ecee26343facd03b")
+    version("2.10", sha256="52b36f726ec00bfca4a2ffc23036d1a2b5f96f0aae5a92fd826be6680c481c20")
+    version("2.2", sha256="7e8683aa74c4454a8cfe3821f405c4439082e24c152b4b834fdb56a117ecaed9")
+
+    variant(
+        "js_engine",
+        values=any_combination_of("node-js", "k8").prohibit_empty_set().with_default("k8"),
+        description="List of javascript engines for which support is enabled",
+    )
+
+    # depends_on("c", type="build")  # generated
+
+    conflicts("target=aarch64:", when="@:2.10")
+    depends_on("zlib-api", type="link")
+    depends_on("py-setuptools", type="build")
+    depends_on("py-cython", type="build")
+
+    variant("jstools", default=False, description="Include Javascript tools (paftools)")
+
+    depends_on("gmake", type="build")
+
+    depends_on("k8", type="run", when="js_engine=k8 +jstools")
+    depends_on("node-js", type="run", when="js_engine=node-js +jstools")
+
+    @run_after("install")
+    def install_minimap2(self):
+        make_arg = []
+        if self.spec.target.family == "aarch64":
+            make_arg.extend(["arm_neon=1", "aarch64=1"])
+        make(*make_arg)
+        mkdirp(prefix.bin)
+        install("minimap2", prefix.bin)
+        if self.spec.satisfies("js_engine=node"):
+            files = glob.glob("./misc/*.js")
+            filter_file(r"k8", "node", *files)
+        install("misc/*.js", prefix.bin)
