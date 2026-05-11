48acb29afb5b544450fb1366077ec67c96f7d934
mercury <mercury@sanger.ac.uk>
2026-05-11 13:21:30 +0000

    Add dnacomb recipe

--- packages/dnacomb/package.py
+++ packages/dnacomb/package.py
@@ -11,6 +11,7 @@ class Dnacomb(Package):
     homepage = "https://github.com/allydunham/dnacomb"
     git = "https://github.com/allydunham/dnacomb.git"
 
+    version("0.6.0rc2", commit="2da8e839a420be5b1104e2e6907e1d7bbe6973aa")
     version("0.6.0rc1", commit="0807765c8a451e1f0f1291261e38067daca14ece")
     version("0.5.0", commit="b5b69722317cd9760770dc24780a436af7ac39bb")
     version("0.4.0", commit="36f10f5de6b7e1d7779e35b1d8e05343a881de14")
