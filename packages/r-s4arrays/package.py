f947a9b0d126ca76532c14b173bd965d89513ad2
Octavio Villas <ov3@sanger.ac.uk>
2026-05-20 14:22:45 +0000

    Add versions to RS4arrays

--- packages/r-s4arrays/package.py
+++ packages/r-s4arrays/package.py
@@ -18,17 +18,22 @@ class RS4arrays(RPackage):
         "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/S4Arrays_1.2.1.tar.gz",
         "https://www.bioconductor.org/packages/3.18/bioc/src/contrib/Archive/S4Arrays/S4Arrays_1.2.1.tar.gz",
     ]
+    git="https://git.bioconductor.org/packages/S4Arrays.git"
 
     version("1.8.0", tag="RELEASE_3_21")
-    version("1.2.1", md5="56e78d721cbf2dc16d12123dca444272")
-    version("1.2.0", md5="c77474b839251d19d31708831de3c1bf")
+    version("1.7.3", commit="41a64ffe40d2e1d2d7092631794de918b5fdab2c")
+    version("1.6.0", tag="RELEASE_3_20")
+    version("1.4.1", tag="RELEASE_3_19")
+    version("1.2.1", tag="RELEASE_3_18")
+    version("1.2.0", commit="bace32150ae231054cadc591a7f7e84f14d8faf0")
     version("1.1.1", commit="6074deee3b0edd47bfde20fa4151c54268e93d63")
     version("0.4.1", commit="09d02b41fbf2a514f176e740ee7ab7bbe2b60151")
 
-    depends_on("r@4.3:", type=("build", "run"), when="@0.5:")
-    depends_on("r-matrix", type=("build", "run"))
-    depends_on("r-abind", type=("build", "run"))
-    depends_on("r-biocgenerics@0.45.2:", type=("build", "run"))
-    depends_on("r-s4vectors", type=("build", "run"))
-    depends_on("r-iranges", type=("build", "run"))
-    depends_on("r-crayon", type=("build", "run"))
+    with default_args(type=("build", "run"))
+    depends_on("r@4.3:", when="@0.5:")
+    depends_on("r-matrix")
+    depends_on("r-abind")
+    depends_on("r-biocgenerics@0.45.2:")
+    depends_on("r-s4vectors")
+    depends_on("r-iranges")
+    depends_on("r-crayon")
