8aae6dc5519d81ffbf67497f691b2490a84d907f
Octavio Villas <ov3@sanger.ac.uk>
2026-06-25 07:57:06 +0000

    Fix pypyarrow build env options being enabled on versions @17:

--- packages/py-pyarrow/package.py
+++ packages/py-pyarrow/package.py
@@ -105,12 +105,15 @@ class PyPyarrow(PythonPackage, CudaPackage):
 
     patch("for_aarch64.patch", when="@0 target=aarch64:")
 
+    # Starting with pyarrow 17+, backend support is built if arrow was built with it
+    @when("@:16")
     def setup_build_environment(self, env):
         env.set("PYARROW_WITH_PARQUET", self.spec.satisfies("+parquet"))
         env.set("PYARROW_WITH_CUDA", self.spec.satisfies("+cuda"))
         env.set("PYARROW_WITH_ORC", self.spec.satisfies("+orc"))
         env.set("PYARROW_WITH_DATASET", self.spec.satisfies("+dataset"))
 
+    @when("@:16")
     def install_options(self, spec, prefix):
         args = []
         if spec.satisfies("+parquet"):
