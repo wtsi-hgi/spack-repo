e61ac03eaa8e21ee18e96ff7c455ffbfd8da3e8c
Octavio Villas <ov3@sanger.ac.uk>
2026-06-29 14:18:26 +0000

    Add RGgh4x version and fix r-ggplot2 dependency

--- packages/r-ggh4x/package.py
+++ packages/r-ggh4x/package.py
@@ -20,12 +20,15 @@ class RGgh4x(RPackage):
 	homepage = "https://github.com/teunbrand/ggh4x"
 	cran = "ggh4x" 
 
+	version("0.3.0", md5="e527f27c933818bc6490b209d8dba54f")
 	version("0.2.8", md5="495cbc3a454dc6e898094d384a58246c")
 
-	depends_on("r-ggplot2@3.4.2:", type=("build", "run"))
-	depends_on("r-gtable", type=("build", "run"))
-	depends_on("r-scales", type=("build", "run"))
-	depends_on("r-vctrs@0.5:", type=("build", "run"))
-	depends_on("r-rlang@1.1:", type=("build", "run"))
-	depends_on("r-lifecycle", type=("build", "run"))
-	depends_on("r-cli", type=("build", "run"))
+	with default_args(type=("build", "run")):
+		depends_on("r-ggplot2@3.5:", when="@0.3.0:")
+		depends_on("r-ggplot2@3.4.2:3.4", when="@0.2.8")
+		depends_on("r-gtable")
+		depends_on("r-scales")
+		depends_on("r-vctrs@0.5:")
+		depends_on("r-rlang@1.1:")
+		depends_on("r-lifecycle")
+		depends_on("r-cli")
