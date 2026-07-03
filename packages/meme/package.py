504052d44db109a5c0a36ad915ce8734e4f86bc0
Octavio Villas <ov3@sanger.ac.uk>
2026-07-03 13:41:33 +0000

    Add meme 5.5.9

--- packages/meme/package.py
+++ packages/meme/package.py
@@ -13,6 +13,7 @@ class Meme(AutotoolsPackage):
     homepage = "https://meme-suite.org"
     url = "http://meme-suite.org/meme-software/5.1.1/meme-5.1.1.tar.gz"
 
+    version("5.5.9", sha256="0406fb7b1dc27f6aab3d6d3a29ecdf617bbdd946690cfa97ca2129bc210bfd12")
     version("5.5.7", sha256="1dca8d0e6d1d36570c1a88ab8dbe7e4b177733fbbeacaa2e8c4674febf57aaf4")
     version("5.5.4", sha256="cda6011c2b855bf2563c4e7a2c255e11e99b5b6e5e73736ff008942507580153")
     version("5.3.0", sha256="b2ddec9db972fcf77b29c7deb62df8b1dd8a6638c13c1aa06a5d563c4a7ff756")
