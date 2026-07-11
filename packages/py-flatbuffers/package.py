6e1361fbae38997227689794714b6eab345669fe
Michael Woolnough <mw31@sanger.ac.uk>
2026-07-11 16:23:21 +0000

    Add older version of py-flatbuffers

--- packages/py-flatbuffers/package.py
+++ packages/py-flatbuffers/package.py
@@ -18,6 +18,7 @@ class PyFlatbuffers(PythonPackage):
     version("24.3.25", sha256="de2ec5b203f21441716617f38443e0a8ebf3d25bf0d9c0bb0ce68fa00ad546a4")
     version("24.3.7", sha256="0895c22b9a6019ff2f4de2e5e2f7cd15914043e6e7033a94c0c6369422690f22")
     version("23.5.26", sha256="9ea1144cac05ce5d86e2859f431c6cd5e66cd9c78c558317c7955fb8d4c78d89")
+    version("1.12", sha256="63bb9a722d5e373701913e226135b28a6f6ac200d5cc7b4d919fa38d73b44610")
 
     depends_on("py-setuptools", type="build")
     # setuptools 70.0.0 turned the invalid license_files '../LICENSE' pattern
