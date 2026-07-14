b9ca2c46252d6deef09fb206e4b8200026fa1f18
Sky Haines-Bass <sh55@sanger.ac.uk>
2026-07-14 13:18:00 +0000

    add tci.sh

---
+++ tci.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -euo pipefail
+
+cd ../spack-repo-1
+main="main"
+pr="$(git branch --show-current)"
+
+PACKAGES="$1";
+echo "Packages to check: $PACKAGES";
+
+declare -a failed_installs=();
+
+for pkg in $PACKAGES; do
+    # Gather pkg info
+    git checkout "$main" -q;
+
+    v1=$(spack info "$pkg" | sed -n '/^Safe versions:/,/^$/p' | sed '1d;$d');
+
+    git checkout "$pr" -q;
+
+    v2=$(spack info "$pkg" | sed -n '/^Safe versions:/,/^$/p' | sed '1d;$d');
+
+    # Build maps of pkg version -> link
+    declare -A map1=();
+    declare -A map2=();
+
+    while IFS=' ' read -r key value; do 
+        map1["$key"]="$value";
+    done <<< "$v1";
+
+    while IFS=' ' read -r key value; do 
+        map2["$key"]="$value";
+    done <<< "$v2";
+
+    # Find new versions
+    declare -a toinstall=();
+    declare -a tocheck=();
+
+    for key in "${!map2[@]}"; do
+        if [[ ! -v map1[$key] ]] || [[ "${map1[$key]}" != "${map2[$key]}" ]]; then
+            toinstall+=("$key");
+        else
+            tocheck+=("$key");
+        fi;
+    done;
+
+    # Identify any versions with changed dependencies
+    for version in "${tocheck[@]}"; do
+        pr_deps=$(awk '
+        /^Run Dependencies:/ {f=1; next}
+        f && /^\[/ {p=1}
+        p {print}
+        p && /\]$/ {exit}
+    ' <<< "$(spack info "$pkg@$version")")
+        
+        git checkout "$main" -q
+        main_deps=$(awk '
+        /^================/ {f=1; next}
+        f && /^\[/ {p=1}
+        p {print}
+        p && /\]$/ {exit}
+    ' <<< "$(spack info "$pkg@$version")")
+
+        git checkout "$pr" -q;
+
+        if [[ "$pr_deps" != "$main_deps" ]]; then
+            echo "dependencies changed, should check install for $version"
+            toinstall+=("$version")
+        fi
+    done
+
+    # Uninstall and reinstall required versions
+    for version in "${toinstall[@]}"; do
+        spec="$pkg@$version";
+
+        if spack find "$spec" >/dev/null 2>&1; then
+            echo "Uninstalling $spec";
+            spack uninstall "$spec" --yes-to-all;
+        else
+            echo "$spec not installed, skipping uninstall";
+        fi;
+
+        echo "Installing $spec";
+        if ! spack install "$spec"; then
+            failed_installs+=($spec);
+        fi;
+    done;
+done;
+
+if [ ${#failed_installs[@]} -eq 0 ]; then
+    echo "No failures"
+else
+    echo "Failed installs:"
+    for pkg in "${!failed_installs[@]}"; do
+        echo "${failed_installs[$pkg]}"
+    done
+    exit 1
+fi
