2f0c13bd600e330614739e134447485a0d2f1670
Octavio Villas <ov3@sanger.ac.uk>
2026-08-04 10:21:43 +0000

    Fix r-ggally r-ggstats

--- packages/r-ggstats/package.py
+++ packages/r-ggstats/package.py
@@ -17,15 +17,16 @@ class RGgstats(RPackage):
 	homepage = "https://larmarange.github.io/ggstats/"
 	cran = "ggstats" 
 
+	version("0.9.0", sha256="610f3779fdef48795b04e9cd85e33a62c43fd4b7147cc429b13b1f96594f0c84")
 	version("0.5.1", md5="d615412a8c42e91abadd45f5d88c49a0")
 
-	depends_on("r-broom-helpers@1.14:", type=("build", "run"))
+	depends_on("r-broom-helpers@1.14:", type=("build", "run"), when="@:0.5.1")
+	depends_on("r-magrittr", type=("build", "run"), when="@:0.5.1")
 	depends_on("r-cli", type=("build", "run"))
 	depends_on("r-dplyr", type=("build", "run"))
 	depends_on("r-forcats", type=("build", "run"))
 	depends_on("r-ggplot2@3.4:", type=("build", "run"))
 	depends_on("r-lifecycle", type=("build", "run"))
-	depends_on("r-magrittr", type=("build", "run"))
 	depends_on("r-patchwork", type=("build", "run"))
 	depends_on("r-purrr", type=("build", "run"))
 	depends_on("r-rlang", type=("build", "run"))
