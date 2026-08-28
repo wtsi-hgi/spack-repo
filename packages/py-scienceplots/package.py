1e0d7c2a7a7560d58fec196a05b4f8adb8b4f229
mercury <mercury@sanger.ac.uk>
2026-08-28 11:35:36 +0000

    Fix import tests for py-sysmexcbctools stack

--- packages/py-scienceplots/package.py
+++ packages/py-scienceplots/package.py
@@ -3,6 +3,8 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
+import os
+
 from spack.package import *
 
 
@@ -28,4 +30,26 @@ class PyScienceplots(PythonPackage):
     def install_test(self):
         with working_dir("spack-test", create=True):
             python = self.spec["python"].command
-            python("-c", "import scienceplots")
+            pyver = self.spec["python"].version.up_to(2)
+            site_packages = join_path(self.prefix, f"lib/python{pyver}", "site-packages")
+            env = os.environ.copy()
+            env["PYTHONPATH"] = (
+                f"{site_packages}{os.pathsep}{env['PYTHONPATH']}"
+                if env.get("PYTHONPATH")
+                else site_packages
+            )
+
+            lib_dirs = []
+            for dep in self.spec.traverse(root=False):
+                for subdir in ("lib", "lib64"):
+                    candidate = join_path(dep.prefix, subdir)
+                    if os.path.isdir(candidate):
+                        lib_dirs.append(candidate)
+            if lib_dirs:
+                env["LD_LIBRARY_PATH"] = (
+                    f"{os.pathsep.join(lib_dirs)}{os.pathsep}{env['LD_LIBRARY_PATH']}"
+                    if env.get("LD_LIBRARY_PATH")
+                    else os.pathsep.join(lib_dirs)
+                )
+
+            python("-c", "import scienceplots", env=env)
