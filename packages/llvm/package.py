71b3a59804dae8a7eea3f339d413b0e029ae0ab5
Octavio Villas <ov3@sanger.ac.uk>
2026-09-03 08:17:47 +0000

    Remove main branch tag from llvm and fix py-numba 0.63.0

--- packages/llvm/package.py
+++ packages/llvm/package.py
@@ -34,7 +34,6 @@ class Llvm(CMakePackage, CudaPackage):
 
     family = "compiler"  # Used by lmod
 
-    version("main", branch="main")
     version("20.1.8", sha256="a6cbad9b2243b17e87795817cfff2107d113543a12486586f8a055a2bb044963")
     version("18.1.3", sha256="fc5a2fd176d73ceb17f4e522f8fe96d8dde23300b8c233476d3609f55d995a7a")
     version("17.0.4", sha256="46200b79f52a02fe26d0a43fd856ab6ceff49ab2a0b7c240ac4b700a6ada700c")
