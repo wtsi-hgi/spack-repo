1fd254feb651054321729cf5cd0908e574bb5845
Octavio Villas <ov3@sanger.ac.uk>
2026-09-09 08:44:35 +0000

    Change source of erlier versions due to missing req file

--- packages/py-cradle/package.py
+++ packages/py-cradle/package.py
@@ -11,11 +11,12 @@ class PyCradle(PythonPackage):
 
     homepage = "https://github.com/ReddyLab/CRADLE"
     pypi = "CRADLE/CRADLE-0.30.0.tar.gz"
+    git = "https://github.com/ReddyLab/CRADLE.git"
 
     license("MIT")
 
-    version("0.19.0", sha256="e35b3f9feef5045d22011d8b4062cea5696d09750c4cd48cf8de412978c4b8d5")
-    version("0.24.0", sha256="e2580add8badd7068210cc22702de94fb254a92bab4b1b9fe9a07bd476b739a6")
+    version("0.19.0", commit="f065feb847e46dc8d46dc73f82e2fbcbe9ba7e77")
+    version("0.24.0", tag="v0.24.0", commit="27a1c0d9c0f3a6335ccfcb21d277de289ab10c33")
     version("0.24.2", sha256="a93934e29e2918a2373220a93e617d5c1343f4256a8f33aecbfcd2ee79d77a58")
     version("0.25.1", sha256="3c8e350e6f75dc2d1a1c985354070c40ddcee8db0bf4943d5aee5b2aa1052dbc")
     version("0.26.1", sha256="080412fea43a31293453ba59297f5f47dc0fba8981ebb22d2d25cdbf2f14b55e")
