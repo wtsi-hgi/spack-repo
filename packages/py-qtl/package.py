5179f4b0b93b5140fc2d6f313ba0528ed1c814a2
Octavio Villas <ov3@sanger.ac.uk>
2026-09-14 10:38:14 +0000

    Fix py-qtl and py-tensorqtl

--- packages/py-qtl/package.py
+++ packages/py-qtl/package.py
@@ -11,10 +11,13 @@ class PyQtl(PythonPackage):
     homepage = "https://github.com/broadinstitute/pyqtl"
     pypi = "qtl/qtl-0.1.8.tar.gz"
 
+    version("0.1.10", sha256="7de75e407627052ba8b74e60ebeebe0e9e911140c58d2026047f0057642e353c")
     version("0.1.8", sha256="8fdb99cda1ceff578a233db6c15a944fa57b43a2826af41c292e36848906117b")
 
     depends_on("py-setuptools", type="build")
+    depends_on("py-setuptools@61:", when="@0.1.10:", type="build")
     depends_on("py-numpy", type=("build", "run"))
+    depends_on("py-numpy@:1", when="@:0.1.8", type=("build", "run"))
     depends_on("py-pandas", type=("build", "run"))
     depends_on("py-scipy", type=("build", "run"))
     depends_on("py-matplotlib", type=("build", "run"))
